75e58d3ab2c1738fe4422827a42fc0f49b88b4d3
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-08-05 21:05:41 -0300

    Update __init__.py

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,4 +3,4 @@ from lib._userquality import UserMetricsOptions
 from lib._dectimegraphs import DectimeGraphsOptions
 from lib._tilequality import QualityAssessmentOptions
 from lib.get_tiles import GetTilesOptions
-from lib.util import load_json, save_json
+from lib.utils.util import load_json, save_json
