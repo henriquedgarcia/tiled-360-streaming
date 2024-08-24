e042caed4731ca63a8623bced39cfe691f14e387
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-08-24 17:34:24 -0300

    update decoder

--- lib/utils/decode_utils.py
+++ lib/utils/decode_utils.py
@@ -1,20 +1,49 @@
+from typing import Any
+
 from config.config import config
 from lib.assets.context import ctx
+from lib.assets.logger import logger
 from lib.assets.paths import paths
-from lib.utils.segmenter_utils import check_chunk_file
-from lib.utils.util import get_times
+from lib.utils.segmenter_utils import segment
+from lib.utils.util import get_times, decode_file
 
 
-def skip_decode():
-    try:
+def make_decode():
+    for ctx.turn in range(config.decoding_num):
+        for _ in iter_decode():
+            decode()
+
+
+def iter_decode():
+    for ctx.name in ctx.name_list:
+        for ctx.projection in ctx.name_list:
+            for ctx.quality in ctx.quality_list:
+                for ctx.tiling in ctx.tiling_list:
+                    for ctx.tile in ctx.tile_list:
+                        for ctx.chunk in ctx.chunk_list:
+                            yield
+
+
+def decode():
+    check_decode()
+    if (logger.get_status('decode_ok')
+            or not logger.get_status('segments_ok')):
+        return
+
+    print(f'==== Decoding {paths.segment_video} - Turn {ctx.turn} ====')
+
+    stdout = decode_file(paths.segment_video, threads=1)
+    with paths.dectime_log.open('a') as f:
+        f.write(f'\n==========\n{stdout}')
+    check_decode()
+
+
+def check_decode():
+    if not logger.get_status('decode_ok'):
         check_dectime_log()
-    except FileNotFoundError:
-        try:
-            check_chunk_file()
-        except FileNotFoundError:
-            return True
-        return False
-    return True  # all ok
+
+    if not logger.get_status('segments_ok'):
+        segment()
 
 
 def clean_dectime_log():
@@ -22,8 +51,13 @@ def clean_dectime_log():
 
 
 def check_dectime_log():
-    dectime_log_content = paths.dectime_log.read_text(encoding='utf-8')
-    ctx.turn = len(get_times(dectime_log_content))
-    if ctx.turn < config.decoding_num:
-        raise FileNotFoundError
+    try:
+        dectime_log_content = paths.dectime_log.read_text(encoding='utf-8')
+        turn = len(get_times(dectime_log_content))
+        if ctx.turn < config.decoding_num:
+            logger.update_status('decode_ok', True)
+            return
+    except FileNotFoundError:
+        clean_dectime_log()
+    logger.update_status('decode_ok', True)
     print(f'\tDecoded {ctx.turn}.')
