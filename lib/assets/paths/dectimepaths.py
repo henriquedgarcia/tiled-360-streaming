8416a46a9e85e10c4e711305eb69383f6f029237
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-09-16 20:20:21 -0300

    Update dectimepaths.py

--- lib/assets/paths/dectimepaths.py
+++ lib/assets/paths/dectimepaths.py
@@ -1,18 +1,23 @@
 from pathlib import Path
 
-from lib.assets.context import ctx
-from lib.assets.paths import segmenter_paths, base_paths
+from config.config import Config
+from lib.assets.context import Context
+from lib.assets.paths.basepaths import BasePaths
+from lib.assets.paths.segmenterpaths import SegmenterPaths
 
 
 class DectimePaths:
+    def __init__(self, config: Config, context: Context, segmenter_paths: SegmenterPaths):
+        self.config = config
+        self.ctx = context
+        self.base_paths = BasePaths(config, context)
+        self.segmenter_paths = segmenter_paths
+
     @property
     def dectime_log(self) -> Path:
-        chunk = int(str(ctx.chunk))
-        return segmenter_paths.chunk_video.with_name(f'tile{ctx.tile}_{chunk:03d}_dectime.log')
+        chunk = int(str(self.ctx.chunk))
+        return self.segmenter_paths.chunk_video.with_name(f'tile{self.ctx.tile}_{chunk:03d}_dectime.log')
 
     @property
     def dectime_result_json(self) -> Path:
-        return base_paths.results_folder / f'time_{ctx.name}.json'
-
-
-dectime_paths = DectimePaths()
+        return self.base_paths.results_folder / f'time_{self.ctx.name}.json'
