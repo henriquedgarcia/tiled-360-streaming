3f2a9b3e4462c9e43861777bad9916af1987d39e
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-09-16 20:20:34 -0300

    Update segmenter.py

--- lib/segmenter.py
+++ lib/segmenter.py
@@ -1,9 +1,8 @@
 from lib.utils.segmenter_utils import create_segments
-from .assets.context import ctx
 from .assets.worker import Worker
 
 
 class Segmenter(Worker):
     def main(self):
-        ctx.quality_list = ['0'] + ctx.quality_list
+        self.ctx.quality_list = ['0'] + self.ctx.quality_list
         create_segments()
