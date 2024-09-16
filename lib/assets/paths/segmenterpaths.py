4c639b760bf689e6715ae1b3133da218683fb135
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-09-16 20:20:25 -0300

    Update segmenterpaths.py

--- lib/assets/paths/segmenterpaths.py
+++ lib/assets/paths/segmenterpaths.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 
-from lib.assets.context import ctx
-from lib.assets.paths.basepaths import base_paths
+from config.config import Config
+from lib.assets.context import Context
+from lib.assets.paths.basepaths import BasePaths
 
 
 class SegmenterPaths:
-    def ___segments_files___(self):
-        ...
+    def __init__(self, config: Config, context: Context):
+        self.config = config
+        self.ctx = context
+        self.base_paths = BasePaths(config, context)
 
     @property
     def lossless_video(self) -> Path:
-        return base_paths.lossless_folder / ctx.projection / f'{ctx.name}.mp4'
+        return self.base_paths.lossless_folder / self.ctx.projection / f'{self.ctx.name}.mp4'
 
     @property
     def lossless_log(self) -> Path:
@@ -18,7 +21,7 @@ class SegmenterPaths:
 
     @property
     def tile_video(self) -> Path:
-        return base_paths.tiles_folder / base_paths.basename1 / f'tile{ctx.tile}.mp4'
+        return self.base_paths.tiles_folder / self.base_paths.basename1 / f'tile{self.ctx.tile}.mp4'
 
     @property
     def tile_log(self) -> Path:
@@ -26,20 +29,17 @@ class SegmenterPaths:
 
     @property
     def segmenter_log(self) -> Path:
-        return base_paths.segmenter_folder / base_paths.basename1 / f'tile{ctx.tile}_segmenter.log'
+        return self.base_paths.segmenter_folder / self.base_paths.basename1 / f'tile{self.ctx.tile}_segmenter.log'
 
     @property
     def chunks_folder(self) -> Path:
-        return base_paths.segmenter_folder / base_paths.basename2
+        return self.base_paths.segmenter_folder / self.base_paths.basename2
 
     @property
     def chunk_video(self) -> Path:
-        chunk = int(ctx.chunk)
-        return self.chunks_folder / f'tile{ctx.tile}_{chunk:03d}.hevc'
+        chunk = int(self.ctx.chunk)
+        return self.chunks_folder / f'tile{self.ctx.tile}_{chunk:03d}.hevc'
 
     @property
     def bitrate_result_json(self) -> Path:
-        return base_paths.results_folder / f'rate_{ctx.name}.json'
-
-
-segmenter_paths = SegmenterPaths()
+        return self.base_paths.results_folder / f'rate_{self.ctx.name}.json'
