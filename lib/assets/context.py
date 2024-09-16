dcaca90aeedb56fd8d4d7b85d44748a6464f16d0
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-09-16 16:44:41 -0300

    Update context.py

--- lib/assets/context.py
+++ lib/assets/context.py
@@ -4,7 +4,6 @@ from config.config import config
 from lib.utils.util import splitx
 from .lazyproperty import LazyProperty
 
-from lib.assets.autodict import AutoDict
 
 class Context:
     name: str = None
@@ -17,7 +16,7 @@ class Context:
     metric: str = None
     turn: str = None
     attempt: int = None
-    projection_dict = AutoDict()
+    projection_dict = dict
 
     factors_list = ['name', 'projection', 'quality', 'tiling', 'tile', 'chunk',
                     'user', 'metric', 'attempt']
