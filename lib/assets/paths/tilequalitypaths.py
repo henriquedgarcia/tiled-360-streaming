c24a011deb819b3f4c7a2b846cce079fee9b1202
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-09-18 18:42:25 -0300

    Update tilequalitypaths.py

--- lib/assets/paths/tilequalitypaths.py
+++ lib/assets/paths/tilequalitypaths.py
@@ -1,20 +1,25 @@
 from pathlib import Path
 
-from lib.assets.context import ctx
-from lib.assets.paths import segmenter_paths, base_paths
+from lib.assets.paths.basepaths import BasePaths
+from lib.assets.paths.segmenterpaths import SegmenterPaths
 from lib.utils.context_utils import context_quality
 
 
 class TileQualityPaths:
+    def __init__(self, config, ctx):
+        self.config = config
+        self.ctx = ctx
+        self.base_paths = BasePaths(self.config, self.ctx)
+        self.segmenter_paths = SegmenterPaths(self.config, self.ctx)
+
     @property
     def reference_chunk(self):
-        with context_quality('0', 'crf'):
-            chunk_video_paths = segmenter_paths.chunk_video
+        with context_quality(self.ctx, self.config, '0', 'crf'):
+            chunk_video_paths = self.segmenter_paths.chunk_video
         return chunk_video_paths
 
     @property
     def quality_result_json(self) -> Path:
-        return base_paths.results_folder / f'quality_{ctx.name}.json'
+        return self.base_paths.results_folder / f'quality_{self.ctx.name}.json'
 
 
-tilequality_paths = TileQualityPaths()
