b518be110248d44e0419a869ba637bf11ce958aa
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-09-30 18:42:15 -0300

    Update decode.py

--- lib/decode.py
+++ lib/decode.py
@@ -1,26 +1,42 @@
-from lib.assets.errors import AbortError, DecodeOkError
+from lib.assets.errors import AbortError, DecodeOkError, InsufficientDecodingError
 from lib.assets.paths.dectimepaths import DectimePaths
 from lib.assets.paths.segmenterpaths import SegmenterPaths
 from lib.assets.worker import Worker
-from lib.segmenter import assert_one_chunk_video
-from lib.utils.worker_utils import decode_video, print_error, count_decoding
+from lib.segmenter import assert_chunk
+from lib.utils.worker_utils import decode_video, print_error, get_times
+
+
+def get_turn(dectime_log):
+    try:
+        turn = len(get_times(dectime_log, only_count=True))
+    except FileNotFoundError:
+        print('ERROR: FileNotFoundError. Return 0.')
+        turn = 0
+    return turn
 
 
 class Decode(Worker):
     turn: int
     segmenter_paths: SegmenterPaths
     dectime_paths: DectimePaths
+    changes: bool
 
     def main(self):
         self.segmenter_paths = SegmenterPaths(config=self.config, context=self.ctx)
         self.dectime_paths = DectimePaths(config=self.config, context=self.ctx, segmenter_paths=self.segmenter_paths)
+        self.decode_chunks()
+
+    def decode_chunks(self):
         for self.ctx.attempt in range(self.config.decoding_num):
+            self.changes = False
             for _ in self.iter_decode():
                 print(f'==== Decoding {self.ctx} ====')
                 try:
                     self.decode()
                 except (DecodeOkError, AbortError) as e:
                     print_error(f'\t{e.args[0]}')
+            if not self.changes:
+                break
 
     def iter_decode(self):
         for self.ctx.name in self.ctx.name_list:
@@ -31,101 +47,63 @@ class Decode(Worker):
                             for self.ctx.chunk in self.ctx.chunk_list:
                                 yield
 
-    def decode_chunks(self):
-        for self.ctx.attempt in range(self.config.decoding_num):
-            for _ in self.iter_decode():
-                print(f'==== Decoding {self.ctx} ====')
-                try:
-                    self.decode()
-                except (DecodeOkError, AbortError) as e:
-                    print_error(f'\t{e.args[0]}')
-
     def decode(self):
         print(f'\tAttempt {self.ctx.attempt + 1}/{self.config.decoding_num}')
-        print(f'\tChecking dectime')
-        self.check_dectime()
+        if not self.status.get_status('dectime_ok'):
+            print(f'\tChecking dectime')
+            self.check_dectime()
 
-        print(f'\tChecking chunks')
-        self.check_chunk()
+        if not self.status.get_status('chunk_ok'):
+            print(f'\tChecking chunks')
+            self.check_chunk()
 
         print(f'\tDecoding {int(self.ctx.turn) + 1}/{self.config.decoding_num}')
         self.decode_decode()
+        self.changes = True
+
+    def check_dectime(self):
+        try:
+            self.assert_dectime_log()
+            self.status.update_status('dectime_ok', True)
+            raise DecodeOkError(f'Dectime is OK. Skipping.')
+        except InsufficientDecodingError:
+            self.status.update_status('decode_turn', self.ctx.turn)
+            self.status.update_status('dectime_ok', False)
+            return
+
+    def assert_dectime_log(self):
+        try:
+            self.ctx.turn = get_turn(self.dectime_paths.dectime_log)
+        except (FileNotFoundError, UnicodeDecodeError):
+            self.ctx.turn = 0
+            self.clean_dectime_log()
+
+        if self.ctx.turn >= self.config.decoding_num: return
+        raise InsufficientDecodingError(f'Dectime is OK. Skipping.')
+
+    def clean_dectime_log(self):
+        self.dectime_paths.dectime_log.unlink(missing_ok=True)
+
+    def check_chunk(self):
+        try:
+            assert_chunk(self.ctx, self.logger, self.segmenter_paths.chunk_video)
+            self.status.update_status('chunk_ok', True)
+        except FileNotFoundError:
+            print_error(f'\tChunks not Found.')
+            self.status.update_status('chunk_ok', False)
+            self.logger.register_log('\tChunk not exist.', self.segmenter_paths.chunk_video)
+            raise AbortError(f'Chunk not exist.')
 
     def decode_decode(self):
         dectime_log = self.dectime_paths.dectime_log
-        folder = dectime_log.parent
-        chunk_video = self.segmenter_paths.chunk_video
 
-        stdout = decode_video(chunk_video, threads=1)
+        stdout = decode_video(self.segmenter_paths.chunk_video, threads=1)
 
-        if not folder.exists():
-            folder.mkdir(parents=True)
+        if not dectime_log.parent.exists():
+            dectime_log.parent.mkdir(parents=True)
 
         with open(dectime_log, 'a') as f:
             f.write('\n' + stdout)
 
         self.check_dectime()
         raise AbortError(f'Decoded {self.ctx.turn} times.')
-
-    def check_dectime(self):
-        try:
-            self.assert_dectime()
-        except FileNotFoundError:
-            self.status.update_status('dectime_ok', False)
-            return
-        finally:
-            self.status.update_status('decode_turn', self.ctx.turn)
-
-        if self.ctx.turn >= self.config.decoding_num:
-            self.status.update_status('dectime_ok', True)
-            raise DecodeOkError(f'Dectime is OK. Skipping.')
-
-    def assert_dectime(self):
-        if not self.status.get_status('dectime_ok'):
-            self.assert_dectime_log()
-
-    def get_turn(self):
-        try:
-            turn = self.ctx.turn = self.get_turn()
-        except FileNotFoundError:
-            print('ERROR: FileNotFoundError. Return 0.')
-            turn = 0
-        return turn
-
-    def assert_dectime_log(self):
-        try:
-            self.ctx.turn = self.get_turn()
-        except FileNotFoundError:
-            self.ctx.turn = 0
-            raise FileNotFoundError('dectime_log not exist.')
-        except UnicodeDecodeError:
-            self.dectime_paths.dectime_log.unlink()
-            self.ctx.turn = 0
-            raise FileNotFoundError('dectime_log UnicodeDecodeError.')
-
-    def check_chunk(self):
-        try:
-            self.assert_chunk()
-        except FileNotFoundError:
-            print_error(f'\tChunks not Found.')
-            self.logger.register_log('\tChunk not exist.', self.segmenter_paths.chunk_video)
-            raise AbortError(f'Chunk not exist.')
-        self.status.update_status('chunk_ok', True)
-
-    def assert_chunk(self):
-        if not self.status.get_status('chunk_ok'):
-            assert_one_chunk_video(self.ctx, self.logger,
-                                   self.segmenter_paths.chunk_video)
-
-    def clean_dectime_log(self):
-        self.dectime_paths.dectime_log.unlink(missing_ok=True)
-
-    def make_decode_cmd(self, threads=1):
-        cmd = (f'bin/ffmpeg -hide_banner -benchmark '
-               f'-codec hevc '
-               f'{"" if not threads else f"-threads {threads} "}'
-               f'-i {self.segmenter_paths.chunk_video.as_posix()} '
-               f'-f null -')
-        cmd = f'bash -c "{cmd}"'
-
-        return cmd
