cc819b29eb5f3589f4827bee9d8d0060e3c6bbe4
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-10-02 17:28:53 -0300

    Update decode.py

--- lib/decode.py
+++ lib/decode.py
@@ -1,21 +1,16 @@
-from lib.assets.errors import AbortError, DecodeOkError, InsufficientDecodingError
+import numpy as np
+
+from lib.assets.ctxinterface import CtxInterface
+from lib.assets.errors import *
+from lib.assets.autodict import AutoDict
 from lib.assets.paths.dectimepaths import DectimePaths
 from lib.assets.paths.segmenterpaths import SegmenterPaths
 from lib.assets.worker import Worker
 from lib.segmenter import assert_chunk
-from lib.utils.worker_utils import decode_video, print_error, get_times
+from lib.utils.worker_utils import decode_video, print_error, count_decoding, save_json, load_json, get_nested_value, get_times
 
 
-def get_turn(dectime_log):
-    try:
-        turn = len(get_times(dectime_log, only_count=True))
-    except FileNotFoundError:
-        print('ERROR: FileNotFoundError. Return 0.')
-        turn = 0
-    return turn
-
-
-class Decode(Worker):
+class Decode(CtxInterface, Worker):
     turn: int
     segmenter_paths: SegmenterPaths
     dectime_paths: DectimePaths
@@ -27,28 +22,30 @@ class Decode(Worker):
         self.decode_chunks()
 
     def decode_chunks(self):
-        for self.ctx.attempt in range(self.config.decoding_num):
+        for self.attempt in range(self.config.decoding_num):
             self.changes = False
             for _ in self.iter_decode():
                 print(f'==== Decoding {self.ctx} ====')
                 try:
                     self.decode()
-                except (DecodeOkError, AbortError) as e:
+                except (DecodeOkError, AbortError, InsufficientDecodingError) as e:
                     print_error(f'\t{e.args[0]}')
+                except ValueError:
+                    self.logger.register_log('Cant decode Chunk.', self.segmenter_paths.chunk_video)
             if not self.changes:
                 break
 
     def iter_decode(self):
-        for self.ctx.name in self.ctx.name_list:
-            for self.ctx.projection in self.ctx.projection_list:
-                for self.ctx.quality in self.ctx.quality_list:
-                    for self.ctx.tiling in self.ctx.tiling_list:
-                        for self.ctx.tile in self.ctx.tile_list:
-                            for self.ctx.chunk in self.ctx.chunk_list:
+        for self.name in self.name_list:
+            for self.projection in self.projection_list:
+                for self.quality in self.quality_list:
+                    for self.tiling in self.tiling_list:
+                        for self.tile in self.tile_list:
+                            for self.chunk in self.chunk_list:
                                 yield
 
     def decode(self):
-        print(f'\tAttempt {self.ctx.attempt + 1}/{self.config.decoding_num}')
+        print(f'\tAttempt {self.attempt + 1}/{self.config.decoding_num}')
         if not self.status.get_status('dectime_ok'):
             print(f'\tChecking dectime')
             self.check_dectime()
@@ -57,9 +54,10 @@ class Decode(Worker):
             print(f'\tChecking chunks')
             self.check_chunk()
 
-        print(f'\tDecoding {int(self.ctx.turn) + 1}/{self.config.decoding_num}')
-        self.decode_decode()
+        print(f'\tDecoding {int(self.turn) + 1}/{self.config.decoding_num}')
         self.changes = True
+        self.decode_decode()
+        print_error(f'Decoded {self.turn} times.')
 
     def check_dectime(self):
         try:
@@ -67,18 +65,18 @@ class Decode(Worker):
             self.status.update_status('dectime_ok', True)
             raise DecodeOkError(f'Dectime is OK. Skipping.')
         except InsufficientDecodingError:
-            self.status.update_status('decode_turn', self.ctx.turn)
+            self.status.update_status('decode_turn', self.turn)
             self.status.update_status('dectime_ok', False)
             return
 
     def assert_dectime_log(self):
         try:
-            self.ctx.turn = get_turn(self.dectime_paths.dectime_log)
+            self.turn = count_decoding(self.dectime_paths.dectime_log)
         except (FileNotFoundError, UnicodeDecodeError):
-            self.ctx.turn = 0
+            self.turn = 0
             self.clean_dectime_log()
 
-        if self.ctx.turn >= self.config.decoding_num: return
+        if self.turn >= self.config.decoding_num: return
         raise InsufficientDecodingError(f'Dectime is OK. Skipping.')
 
     def clean_dectime_log(self):
@@ -106,4 +104,4 @@ class Decode(Worker):
             f.write('\n' + stdout)
 
         self.check_dectime()
-        raise AbortError(f'Decoded {self.ctx.turn} times.')
+
