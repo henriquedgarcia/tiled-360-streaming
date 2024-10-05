4ae5a0d0eaf7ee1f5f5bab91a5c3ecdaa0a79038
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-10-04 22:45:15 -0300

    Update userqualitypaths.py

--- lib/assets/paths/userqualitypaths.py
+++ lib/assets/paths/userqualitypaths.py
@@ -4,73 +4,55 @@ import pandas as pd
 
 from lib.assets.context import Context
 from lib.assets.paths.basepaths import BasePaths
+from lib.assets.ctxinterface import CtxInterface
 
 
-class UserQualityPaths:
+class UserQualityPaths(CtxInterface):
     def __init__(self, context: Context):
         self.ctx = context
         self.config = context.config
         self.base_paths = BasePaths(context)
 
     @property
-    def get_tiles_folder(self) -> Path:
-        return self.base_paths.project_path / 'get_tiles'
-
-    @property
-    def get_tiles_json(self) -> Path:
-        filename = (f'get_tiles_{self.ctx.name}_{self.ctx.projection}'
-                    f'_fov{self.config.fov}.json')
-        return self.get_tiles_folder / filename
-
-    @property
-    def user_tiles_seen_json(self) -> Path:
-        folder = self.get_tiles_folder / self.ctx.name / self.ctx.projection / self.ctx.tiling
-        filename = f'seen_tiles_user{self.ctx.user}.json'
-        return folder / filename
-
-    @property
-    def counter_tiles_json(self):
-        filename = (f'counter_{self.dataset_name}_{self.ctx.projection}_{self.ctx.name}'
-                    f'_fov{self.config.fov}.json')
-        folder = self.get_tiles_folder / 'counter'
-        return folder / filename
-
-    @property
-    def heatmap_tiling(self):
-        filename = (f'heatmap_tiling_{self.dataset_name}_{self.ctx.projection}_{self.ctx.name}_{self.ctx.tiling}'
-                    f'_fov{self.config.fov}.png')
-        folder = self.get_tiles_folder / 'heatmap'
-        folder.mkdir(parents=True, exist_ok=True)
-        return folder / filename
-
-    @property
-    def video_test_folder(self):
-        folder = self.get_tiles_folder / 'video_test'
-        folder.mkdir(parents=True, exist_ok=True)
+    def user_quality_folder(self) -> Path:
+        folder = (self.base_paths.user_quality_folder / 'user_quality'
+                  / self.name / self.projection / self.quality / self.tiling)
         return folder
 
     @property
-    def sph_file(self) -> Path:
-        return self.config.sph_file
+    def user_metrics_json(self) -> Path:
+        filename = f'seen_tiles_user{self.user}.json'
+        return self.user_quality_folder / filename
 
     @property
-    def dataset_json(self) -> Path:
-        return self.config.dataset_file
+    def counter_tiles_folder(self) -> Path:
+        folder = (self.base_paths.user_quality_folder / 'counter_tiles'
+                  / self.name / self.projection / self.quality / self.tiling)
+        return folder
 
     @property
-    def dataset_name(self):
-        return self.config.dataset_file.stem
-
-    _csv_dataset_file: Path
+    def counter_tiles_json(self):
+        filename = f'counter_fov{self.fov}.json'
+        return self.counter_tiles_folder / filename
 
     @property
-    def csv_dataset_file(self) -> Path:
-        return self._csv_dataset_file
+    def heatmap_folder(self) -> Path:
+        folder = (self.base_paths.user_quality_folder / 'heatmap'
+                  / self.name / self.projection / self.quality / self.tiling)
+        return folder
 
-    head_movement: pd.DataFrame
+    @property
+    def heatmap_tiling(self):
+        filename = f'heatmap_tiling_fov{self.config.fov}.png'
+        return self.heatmap_folder / filename
 
-    @csv_dataset_file.setter
-    def csv_dataset_file(self, filename: Path) -> None:
-        self._csv_dataset_file = filename
-        self.head_movement = pd.read_csv(filename,
-                                         names=['timestamp', 'Qx', 'Qy', 'Qz', 'Qw', 'Vx', 'Vy', 'Vz'])
+    @property
+    def video_test_folder(self):
+        folder = (self.base_paths.user_quality_folder / 'video_test'
+                  / self.name / self.projection / self.quality / self.tiling)
+        return folder
+
+    @property
+    def video_test(self):
+        filename = f'video_test_fov{self.config.fov}_{self.name}_{self.projection}_{self.quality}_{self.tiling}.png'
+        return self.video_test_folder / filename
