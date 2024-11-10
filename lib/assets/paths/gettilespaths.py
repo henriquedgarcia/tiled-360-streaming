1ae65b305f20625c4e64642d4a557b33da00570f
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-09 22:34:08 -0300

    Update gettilespaths.py

--- lib/assets/paths/gettilespaths.py
+++ lib/assets/paths/gettilespaths.py
@@ -13,38 +13,34 @@ class GetTilesPaths(CtxInterface):
         self.base_paths = BasePaths(context)
 
     @property
-    def get_tiles_folder(self) -> Path:
-        return self.base_paths.project_path / 'get_tiles'
+    def get_tiles_result_json(self) -> Path:
+        return self.base_paths.results_folder / f'get_tiles_{self.name}_{self.projection}_fov{self.fov}.json'
 
     @property
-    def get_tiles_result_json(self) -> Path:
-        filename = (f'get_tiles_{self.name}_{self.projection}'
-                    f'_fov{self.fov}.json')
-        return self.base_paths.results_folder / filename
+    def user_tiles_seen_folder(self) -> Path:
+        return self.base_paths.get_tiles_folder / self.base_paths.basename_lv3
 
     @property
     def user_tiles_seen_json(self) -> Path:
-        folder = self.get_tiles_folder / self.name / self.projection / self.tiling
-        filename = f'user{int(self.user):02d}.json'
-        return folder / filename
+        return self.user_tiles_seen_folder / f'user{int(self.user):02d}.json'
 
     @property
     def counter_tiles_json(self):
         filename = (f'counter_{self.name}_{self.projection}'
                     f'_fov{self.fov}.json')
-        folder = self.get_tiles_folder / 'counter'
+        folder = self.base_paths.get_tiles_folder / 'counter'
         return folder / filename
 
     @property
     def heatmap_tiling(self):
         filename = (f'heatmap_tiling_{self.dataset_name}_{self.projection}_{self.name}_{self.tiling}'
                     f'_fov{self.fov}.png')
-        folder = self.get_tiles_folder / 'heatmap'
+        folder = self.base_paths.get_tiles_folder / 'heatmap'
         folder.mkdir(parents=True, exist_ok=True)
         return folder / filename
 
     @property
     def video_test_folder(self):
-        folder = self.get_tiles_folder / 'video_test'
+        folder = self.base_paths.get_tiles_folder / 'video_test'
         folder.mkdir(parents=True, exist_ok=True)
         return folder
