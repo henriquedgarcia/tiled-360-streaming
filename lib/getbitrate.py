da42fb270469bfcf37e9da675721646d4aa21f4f
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-09 22:34:31 -0300

    Update getbitrate.py

--- lib/getbitrate.py
+++ lib/getbitrate.py
@@ -1,13 +1,14 @@
-from typing import Any
-
 from lib.assets.autodict import AutoDict
-from lib.assets.worker import Worker
-from lib.utils.worker_utils import save_json, load_json, print_error
-from lib.makechunks import MakeChunks, SegmenterPaths
 from lib.assets.ctxinterface import CtxInterface
+from lib.assets.errors import AbortError
+from lib.assets.worker import Worker
+from lib.makedash import MakeDashPaths
+from lib.utils.context_utils import task
 from lib.utils.worker_utils import get_nested_value
+from lib.utils.worker_utils import save_json, print_error
 
-class GetBitrate(MakeChunks, CtxInterface):
+
+class GetBitrate(Worker, CtxInterface):
     """
        The result dict have a following structure:
        results[video_name][tile_pattern][quality][tile_id][chunk_id]
@@ -21,47 +22,53 @@ class GetBitrate(MakeChunks, CtxInterface):
 
     """
     video_bitrate: AutoDict
+    quality_list: list
+    decodable_paths: MakeDashPaths
 
     def main(self):
-        self.segmenter_paths = SegmenterPaths(self.ctx)
-        self.ctx.quality_list = ['0'] + self.ctx.quality_list
+        self.init()
+        for _ in self.iterate_name_projection_tiling_tile_quality_chunk():
+            with task(self):
+                self.work()
+
+    def init(self):
+        self.decodable_paths = MakeDashPaths(self.ctx)
+        self.quality_list = ['0'] + self.ctx.quality_list
+        self.collect_bitrate()
+
+    def work(self):
         self.collect_bitrate()
 
     def collect_bitrate(self):
         for self.name in self.name_list:
-            if self.segmenter_paths.bitrate_result_json.exists():
+            if self.decodable_paths.bitrate_result_json.exists():
                 print_error(f'\tThe bitrate_result_json exist.')
                 continue
 
             self.video_bitrate = AutoDict()
 
-            for self.projection in self.projection_list:
-                for self.quality in self.quality_list:
-                    for self.tiling in self.tiling_list:
-                        for self.tile in self.tile_list:
-                            for self.chunk in self.chunk_list:
-                                self.bitrate()
+            with task(self):
+                for _ in self.iterate_projection_tiling_tile_quality_chunk():
+                    self.bitrate()
 
             print(f'Saving video_bitrate for {self.name}.')
             save_json(self.video_bitrate,
-                      self.segmenter_paths.bitrate_result_json)
+                      self.decodable_paths.bitrate_result_json)
 
     def bitrate(self):
         print(f'\t{self.ctx}', end='\r')
 
         try:
-            chunk_size = self.segmenter_paths.chunk_video.stat().st_size
+            dash_m4s = self.decodable_paths.dash_m4s.stat().st_size
+            bitrate = 8 * dash_m4s
+            if dash_m4s == 0:
+                self.logger.register_log('BITRATE==0', self.decodable_paths.dash_m4s)
+
         except FileNotFoundError:
-            chunk_size = 0
+            self.logger.register_log('chunk not exist', self.decodable_paths.dash_m4s)
+            raise AbortError('Chunk not exist')
 
-        bitrate = 8 * chunk_size / 1  # chunk duration is 1 second
-
-        if chunk_size == 0:
-            self.logger.register_log('BITRATE==0',
-                                     self.segmenter_paths.chunk_video)
-
-        self.set_bitrate({'bitrate': bitrate,
-                          })
+        self.set_bitrate({'bitrate': bitrate, })
 
     def get_bitrate(self):
         keys = [self.name, self.projection, self.quality, self.tiling, self.tile, self.chunk]
