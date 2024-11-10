7ee07025ce4e3cc7d3b09211dd1acbb141e3ef79
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-09 22:34:19 -0300

    Update tilequalitypaths.py

--- lib/assets/paths/tilequalitypaths.py
+++ lib/assets/paths/tilequalitypaths.py
@@ -3,22 +3,25 @@ from pathlib import Path
 from lib.assets.context import Context
 from lib.assets.ctxinterface import CtxInterface
 from lib.assets.paths.basepaths import BasePaths
-from lib.assets.paths.segmenterpaths import SegmenterPaths
+from lib.assets.paths.make_decodable_paths import MakeDecodablePaths
 from lib.utils.context_utils import context_quality
 
 
 class ChunkQualityPaths(CtxInterface):
     def __init__(self, context: Context):
         self.ctx = context
-        self.config = context.config
         self.base_paths = BasePaths(context)
-        self.segmenter_paths = SegmenterPaths(context)
+        self.make_decodable_paths = MakeDecodablePaths(context)
 
     @property
     def reference_chunk(self):
         with context_quality(self.ctx, '0', 'qp'):
-            chunk_video_paths = self.segmenter_paths.chunk_video
+            chunk_video_paths = self.decodable_chunk
         return chunk_video_paths
+
+    @property
+    def decodable_chunk(self):
+        return self.make_decodable_paths.decodable_chunk
 
     @property
     def chunk_quality_result_json(self) -> Path:
@@ -26,12 +29,12 @@ class ChunkQualityPaths(CtxInterface):
 
     @property
     def chunk_quality_folder(self) -> Path:
-        folder = self.base_paths.quality_folder / self.base_paths.basename2
+        folder = self.base_paths.quality_folder / self.base_paths.basename_lv4
         return folder
 
     @property
     def chunk_quality_json(self) -> Path:
-        filename = f'tile{self.tile}_{int(self.chunk):03d}.json'
+        filename = f'chunk{int(self.chunk):03d}_{self.config.rate_control}{self.ctx.quality}.json'
         return self.chunk_quality_folder / filename
 
     @property
