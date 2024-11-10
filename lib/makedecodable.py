59bb7f9ecedcfed895797571c0ce5881a0bb8d70
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-09 22:34:42 -0300

    Update makedecodable.py

--- lib/makedecodable.py
+++ lib/makedecodable.py
@@ -18,7 +18,7 @@ class MakeDecodable(Worker, CtxInterface):
 
     def main(self):
         self.init()
-        for _ in self.iterate_name_projection_quality_tiling_tile_chunk():
+        for _ in self.iterate_name_projection_tiling_tile_quality_chunk():
             with task(self):
                 self.work()
 
@@ -29,38 +29,34 @@ class MakeDecodable(Worker, CtxInterface):
         self.run()
 
     def run(self):
-        run_command(self.cmd, folder=self.make_decodable_path.decodable_folder,
+        run_command(self.cmd, folder=self.decodable_folder,
                     ui_prefix='\t')
 
     def make_decodable_cmd(self):
-        dash_init = self.make_decodable_path.dash_init
-        dash_m4s = self.make_decodable_path.dash_m4s
-        decodable_chunk = self.make_decodable_path.decodable_chunk
-
-        self.cmd = (f'bash -c "cat {dash_init.as_posix()} {dash_m4s.as_posix()} '
-                    f'> {decodable_chunk.as_posix()}"')
+        self.cmd = (f'bash -c "cat {self.dash_init.as_posix()} {self.dash_m4s.as_posix()} '
+                    f'> {self.decodable_chunk.as_posix()}"')
 
     def check_dash(self):
         msg = []
-        if not self.make_decodable_path.dash_m4s.exists():
+        if not self.dash_m4s.exists():
             msg += ['dash_m4s not exist']
-            self.logger.register_log('Dash M4S not found.', self.make_decodable_path.dash_m4s)
-        if not self.make_decodable_path.dash_init.exists():
+            self.logger.register_log('Dash M4S not found.', self.dash_m4s)
+        if not self.dash_init.exists():
             msg += ['dash_init not exist']
-            self.logger.register_log('Dash init not found.', self.make_decodable_path.dash_m4s)
+            self.logger.register_log('Dash init not found.', self.dash_m4s)
         if msg:
             raise AbortError('/'.join(msg))
 
     def check_decodable(self):
         try:
-            chunk_size = self.make_decodable_path.decodable_chunk.stat().st_size
+            chunk_size = self.decodable_chunk.stat().st_size
             if self.decode_check:
                 self.check_one_chunk_decode()
         except FileNotFoundError:
             return 'decodable_chunk not found. Continue.'
         if chunk_size == 0:
-            self.logger.register_log('Chunk size is 0.', self.make_decodable_path.decodable_chunk)
-            self.make_decodable_path.decodable_chunk.unlink()
+            self.logger.register_log('Chunk size is 0.', self.decodable_chunk)
+            self.decodable_chunk.unlink()
             return 'decodable_chunk size 0. Cleaning and continue.'
         raise AbortError(f'decodable_chunk is OK.')
 
@@ -69,8 +65,24 @@ class MakeDecodable(Worker, CtxInterface):
             raise AbortError(f'Decoding check is ok.')
 
         print(f'\tDecoding check')
-        decodable_chunk = self.make_decodable_path.decodable_chunk
+        decodable_chunk = self.decodable_chunk
         stdout = decode_video(decodable_chunk, ui_prefix='\t')
         if "frame=   30" not in stdout:  # specific for ffmpeg 5.0
             self.logger.register_log(f'Chunk Decode Error.', decodable_chunk)
             raise FileNotFoundError(f'Chunk Decode Error.')
+
+    @property
+    def decodable_folder(self):
+        return self.make_decodable_path.decodable_folder
+
+    @property
+    def dash_init(self):
+        return self.make_decodable_path.dash_init
+
+    @property
+    def dash_m4s(self):
+        return self.make_decodable_path.dash_m4s
+
+    @property
+    def decodable_chunk(self):
+        return self.make_decodable_path.decodable_chunk
