2b3627679f24c57835a7657719e2513619d4ec22
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-20 17:16:12 -0300

    Update worker.py

--- lib/assets/worker.py
+++ lib/assets/worker.py
@@ -85,6 +85,15 @@ class Worker(ABC, CtxInterface):
                                 self.ctx.iterations += 1
                                 yield
 
+    def iterate_projection_tiling_tile_quality_chunk(self):
+        for self.projection in self.projection_list:
+            for self.tiling in self.tiling_list:
+                for self.tile in self.tile_list:
+                    for self.quality in self.quality_list:
+                        for self.chunk in self.chunk_list:
+                            self.ctx.iterations += 1
+                            yield
+
     def iterate_projection_tiling_tile(self):
         for self.projection in self.projection_list:
             for self.tiling in self.tiling_list:
