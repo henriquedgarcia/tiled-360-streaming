e273eeb79a5b51988d15cc1507a5e92c8da295f8
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-24 15:49:32 -0300

    Update getget_tiles.py

--- lib/getget_tiles.py
+++ lib/getget_tiles.py
@@ -1,15 +1,12 @@
 from contextlib import contextmanager
 from typing import Any
 
-from pywin.mfc.object import Object
-
 from lib.assets.autodict import AutoDict
 from lib.assets.ctxinterface import CtxInterface
 from lib.assets.errors import AbortError
 from lib.assets.worker import Worker, ProgressBar
 from lib.get_tiles import GetTilesPaths
-from lib.utils.worker_utils import get_nested_value
-from lib.utils.worker_utils import save_json, load_json, print_error
+from lib.utils.worker_utils import get_nested_value, save_json, load_json, print_error
 
 
 class GetGetTiles(Worker, CtxInterface):
@@ -17,9 +14,14 @@ class GetGetTiles(Worker, CtxInterface):
     get_tiles_paths: GetTilesPaths
 
     def iter_proj_tiling_tile_qlt_chunk(self):
+        class_name = self.__class__.__name__
+        self.total = (len(self.projection_list)
+                      * len(self.tiling_list) * len(self.users_list))
+        t = ProgressBar(total=self.total, desc=class_name)
         for self.projection in self.projection_list:
             for self.tiling in self.tiling_list:
                 for self.user in self.users_list:
+                    t.update(f'{self.ctx}')
                     self.ctx.iterations += 1
                     yield
 
@@ -27,28 +29,23 @@ class GetGetTiles(Worker, CtxInterface):
 
     def init(self):
         self.get_tiles_paths = GetTilesPaths(self.ctx)
-        self.total = (181 * len(self.projection_list)
-                      * len(self.quality_list) * len(self.chunk_list))
 
     @contextmanager
     def task(self):
         class_name = self.__class__.__name__
         print(f'==== {class_name} {self.ctx} ====')
         self.get_tiles_result = AutoDict()
-        t = ProgressBar(total=self.total, desc=class_name)
 
         try:
-            for _ in self.iter_proj_tiling_tile_qlt_chunk():
-                t.update(f'{self.ctx}')
-                yield
-
+            yield
         except FileNotFoundError as e:
             print_error('Chunk not Found.')
+            return
         except AbortError as e:
             print_error(f'\t{e.args[0]}')
+            return
 
         save_json(self.get_tiles_result, self.get_tiles_paths.get_tiles_result_json)
-        del t
 
     def main(self):
         for self.name in self.name_list:
@@ -57,8 +54,9 @@ class GetGetTiles(Worker, CtxInterface):
                 continue
 
             with self.task():
-                user_tiles_seen = load_json(self.get_tiles_paths.user_tiles_seen_json)
-                self.set_get_tiles(user_tiles_seen)
+                for _ in self.iter_proj_tiling_tile_qlt_chunk():
+                    user_tiles_seen = load_json(self.get_tiles_paths.user_tiles_seen_json)
+                    self.set_get_tiles(user_tiles_seen)
 
             save_json(self.get_tiles_result, self.get_tiles_paths.get_tiles_result_json)
 
