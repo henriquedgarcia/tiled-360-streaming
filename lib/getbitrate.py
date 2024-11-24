8d186f82f6eb733c2fbee4ef4f3a346307b88c9d
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-24 17:55:12 -0300

    Update getbitrate.py

--- lib/getbitrate.py
+++ lib/getbitrate.py
@@ -1,10 +1,10 @@
-from tqdm import tqdm
+from contextlib import contextmanager
 
 from lib.assets.autodict import AutoDict
 from lib.assets.ctxinterface import CtxInterface
-from lib.assets.worker import Worker
+from lib.assets.errors import AbortError
+from lib.assets.worker import Worker, ProgressBar
 from lib.makedash import MakeDashPaths
-from lib.utils.context_utils import task
 from lib.utils.worker_utils import get_nested_value, save_json, print_error
 
 
@@ -18,39 +18,56 @@ class GetBitrate(Worker, CtxInterface):
     """
     video_bitrate: AutoDict
     decodable_paths: MakeDashPaths
-    t: tqdm
+    total: int
+    t: ProgressBar
 
     def main(self):
-        self.decodable_paths = MakeDashPaths(self.ctx)
+        self.init()
 
         for self.name in self.name_list:
-            print(f'==== {self.__class__.__name__} {self.name} ====')
-            if self.decodable_paths.bitrate_result_json.exists():
-                print_error(f'\tThe bitrate_result_json exist.')
-                continue
-
-            self.video_bitrate = AutoDict()
-            self.t = tqdm(total=(181
-                                 * len(self.projection_list)
-                                 * len(self.quality_list)
-                                 * len(self.chunk_list)),
-                          desc=f'    {self.__class__.__name__}')
-            for _ in self.iterate_projection_tiling_tile():
-                with task(self, verbose=False):
+            with self.task():
+                for _ in self.iterator():
                     self.work()
-            self.t.close()
 
-            print(f'\tSaving video_bitrate for {self.name}.')
-            save_json(self.video_bitrate,
-                      self.decodable_paths.bitrate_result_json)
+    def init(self):
+        self.decodable_paths = MakeDashPaths(self.ctx)
+        self.total = (181
+                      * len(self.projection_list)
+                      * len(self.quality_list)
+                      * len(self.chunk_list))
+
+    def iterator(self):
+        self.t = ProgressBar(total=self.total, desc=self.__class__.__name__)
+        for self.projection in self.projection_list:
+            for self.tiling in self.tiling_list:
+                for self.tile in self.tile_list:
+                    self.ctx.iterations += 1
+                    yield
+        self.t.close()
+
+    @contextmanager
+    def task(self):
+        print(f'==== {self.__class__.__name__} {self.name} ====')
+
+        self.video_bitrate = AutoDict()
+        try:
+            if self.decodable_paths.bitrate_result_json.exists():
+                raise AbortError(f'The bitrate_result_json exist.')
+            yield
+        except AbortError as e:
+            print_error(f'\t{e.args[0]}')
+            return
+
+        print(f'\tSaving for {self.name}.')
+        save_json(self.video_bitrate,
+                  self.decodable_paths.bitrate_result_json)
 
     def work(self):
         bitrate = AutoDict()
 
         for self.quality in self.quality_list:
             for self.chunk in self.chunk_list:
-                self.t.set_postfix_str(f'{self.ctx}')
-                self.t.update()
+                self.t.update(f'{self.ctx}')
                 bitrate[self.quality][self.chunk]['dash_m4s'] = self.decodable_paths.dash_m4s.stat().st_size
             bitrate[self.quality]['dash_init'] = self.decodable_paths.dash_init.stat().st_size
         bitrate['dash_mpd'] = self.decodable_paths.dash_mpd.stat().st_size
