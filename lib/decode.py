87f4b7bc4c7072fbb250d218379a18c297aebff0
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-27 08:33:17 -0300

    Update decode.py

--- lib/decode.py
+++ lib/decode.py
@@ -1,40 +1,67 @@
+from contextlib import contextmanager
+
 from lib.assets.errors import AbortError
 from lib.assets.paths.dectimepaths import DectimePaths
-from lib.assets.worker import Worker
+from lib.assets.worker import Worker, ProgressBar
 from lib.utils.context_utils import task
 from lib.utils.worker_utils import decode_video, count_decoding, print_error
 
 
 class Decode(Worker):
     dectime_paths: DectimePaths
-    changes: bool
     stdout: str
     items = []
 
+    def iter_items(self):
+        t = ProgressBar(total=len(self.items), desc=self.__class__.__name__)
+        for item in self.items:
+            t.update(f'{self.ctx}')
+            (self.name, self.projection, self.tiling,
+             self.tile, self.quality, self.chunk) = item
+            yield
+
+            if self.turn >= 5:
+                self.items.remove(item)
+
     def main(self):
-        self.dectime_paths = DectimePaths(context=self.ctx)
-        self.decode_chunks()
+        self.init()
+        for self.attempt in range(self.config.decoding_num):
+            self.decode_chunks()
 
     def decode_chunks(self):
-        for _ in self.iterate_name_projection_tiling_tile_quality_chunk():
-            self.items.append((self.name, self.projection, self.tiling,
-                               self.tile, self.quality, self.chunk))
+        for _ in self.iter_items():
+            with task(self, verbose=False):
+                self.stdout = decode_video(self.decodable_chunk, threads=1, ui_prefix='\t')
+                self.save()
 
-        for self.attempt in range(self.config.decoding_num):
-            for item in self.items:
-                (self.name, self.projection, self.tiling,
-                 self.tile, self.quality, self.chunk) = item
+    def init(self):
+        self.dectime_paths = DectimePaths(context=self.ctx)
+        self.make_task_list()
 
-                with task(self):
-                    self.work()
-                    if self.turn >= 5:
-                        self.items.remove(item)
+    @contextmanager
+    def task(self):
+        print(f'==== {self.__class__.__name__} {self.ctx} ====')
 
-    def work(self):
-        self.check_dectime()
-        self.check_decodable()
-        self.decode()
+        try:
+            yield
+        except AbortError as e:
+            print_error(f'\t{e.args[0]}')
+            return
+
         self.save()
+
+    def save(self):
+        self.dectime_log.parent.mkdir(parents=True, exist_ok=True)
+        with open(self.dectime_log, 'a') as f:
+            f.write('\n' + self.stdout)
+
+    def iterator(self):
+        total = len(self.name_list) * len(self.projection_list) * 181 * len(self.quality_list) * len(self.chunk_list)
+        t = ProgressBar(total=total, desc=self.__class__.__name__)
+
+        for _ in self.iterate_name_projection_tiling_tile_quality_chunk():
+            t.update(f'{self.ctx}')
+            yield
 
     def check_dectime(self):
         try:
@@ -48,22 +75,18 @@ class Decode(Worker):
         raise AbortError(f'Dectime is OK. {self.turn}/{self.decoding_num} times. Skipping.')
 
     def check_decodable(self):
-        msg = []
         if not self.decodable_chunk.exists():
-            msg += ['decodable_chunk not exist']
-            self.logger.register_log('decodable_chunk not found.', self.decodable_chunk)
-        if msg:
+            msg = 'decodable_chunk not exist'
+            self.logger.register_log(msg, self.decodable_chunk)
             raise AbortError('/'.join(msg))
 
-    def decode(self):
-        self.stdout = decode_video(self.decodable_chunk, threads=1, ui_prefix='\t')
-
-    def save(self):
-        self.dectime_log.parent.mkdir(parents=True, exist_ok=True)
-        with open(self.dectime_log, 'a') as f:
-            f.write('\n' + self.stdout)
-
-        self.changes = True
+    def make_task_list(self):
+        for _ in self.iterator():
+            with self.task():
+                self.check_dectime()
+                self.check_decodable()
+                self.items.append((self.name, self.projection, self.tiling,
+                                   self.tile, self.quality, self.chunk))
 
     @property
     def dectime_log(self):
