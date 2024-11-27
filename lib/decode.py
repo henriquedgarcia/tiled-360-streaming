99cda7a5510b066ec70d5b590de6aec0f596390e
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-11-27 10:26:06 -0300

    Update decode.py

--- lib/decode.py
+++ lib/decode.py
@@ -21,48 +21,22 @@ class Decode(Worker):
 
     def init(self):
         self.dectime_paths = DectimePaths(context=self.ctx)
-        self.make_task_list()
 
-    def make_task_list(self):
-        self.items = AutoDict()
-        self.total = 0
-        for _ in self.iter_ctx('Creating items list'):
-            with self.task1():
-                self.check_dectime()
-                self.check_decodable()
-                self.items[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = None
-                self.total += 1
+    def decode_chunks(self):
+        for _ in self.iter_items():
+            if self.status.get_status('is_ok'):
+                continue
+            self.check_dectime()
+            self.check_decodable()
+            with self.task2(self):
+                self.stdout = decode_video(self.decodable_chunk, threads=1, ui_prefix='\t')
 
-    @property
-    def tiling(self):
-        return self.ctx.tiling
-
-    @tiling.setter
-    def tiling(self, tiling):
-        self.ctx.tiling = tiling
-        self.t.update(f'{self.ctx}')
-
-    def iter_ctx(self, desc):
-        # total = len(self.name_list) * len(self.projection_list) * 181 * len(self.quality_list) * len(self.chunk_list)
-        total = len(self.name_list) * len(self.projection_list) * 181
-        self.t = ProgressBar(total=total, desc=desc)
-        for self.name in self.name_list:
-            for self.projection in self.projection_list:
-                for self.tiling in self.tiling_list:
-                    for self.tile in self.tile_list:
-                        self.t.update(f'{self.ctx}')
-                        for self.quality in self.quality_list:
-                            for self.chunk in self.chunk_list:
-                                self.ctx.iterations += 1
-                                yield
-                        self.quality = self.chunk = None
-
-    @contextmanager
-    def task1(self):
-        try:
+    def iter_items(self):
+        total = len(self.name_list) * len(self.projection_list) * 181 * len(self.quality_list) * len(self.chunk_list)
+        self.t = ProgressBar(total=total, desc=self.__class__.__name__)
+        for _ in self.iterate_name_projection_tiling_tile_quality_chunk():
+            self.t.update(f'{self.ctx}')
             yield
-        except AbortError:
-            return
 
     def check_dectime(self):
         try:
@@ -71,8 +45,8 @@ class Decode(Worker):
             self.turn = 0
             self.dectime_log.unlink(missing_ok=True)
         if self.turn < self.decoding_num:
-            print_error(f'\tDecoded {self.turn} times.')
             return
+        self.status.update_status('is_ok', True)
         raise AbortError()
 
     def check_decodable(self):
@@ -81,36 +55,13 @@ class Decode(Worker):
             self.logger.register_log(msg, self.decodable_chunk)
             raise AbortError()
 
-    def decode_chunks(self):
-        for _ in self.iter_items():
-            with self.task2(self):
-                self.stdout = decode_video(self.decodable_chunk, threads=1, ui_prefix='\t')
-
-    def iter_items(self):
-        self.t = ProgressBar(total=self.total, desc=self.__class__.__name__)
-        pilha = []
-        for item in traverse_items(self.items, pilha):
-            self.name, self.projection, self.tiling, self.tile, self.quality, self.chunk = item
-            self.t.update(f'{self.ctx}')
-
-            yield
-            self.check_turn(item)
-
-    def check_turn(self, item):
-        if self.turn >= 5:
-            result = get_nested_value(self.items, item[:-1])
-            del result[item[-1]]
-
     @contextmanager
     def task2(self):
-        print(f'==== {self.__class__.__name__} {self.ctx} ====')
-
         try:
             yield
         except AbortError as e:
             print_error(f'\t{e.args[0]}')
             return
-
         self.save()
 
     def save(self):
@@ -129,13 +80,3 @@ class Decode(Worker):
     @property
     def dectime_folder(self):
         return self.dectime_paths.dectime_folder
-
-
-def traverse_items(x: dict, pilha):
-    if x is None:
-        yield pilha
-    elif isinstance(x, dict):
-        for key in x:
-            pilha.append(key)
-            traverse_items(x[key], pilha)
-            pilha.pop()
