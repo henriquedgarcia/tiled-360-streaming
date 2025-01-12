b2c415621823c980ddfaef4ed4b6722fa9b982d6
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-01-12 17:21:21 -0300

    Update mountframe.py

--- lib/assets/mountframe.py
+++ lib/assets/mountframe.py
@@ -9,6 +9,7 @@ from lib.utils.util import build_projection, idx2xy, iter_video
 
 class MountFrame(CtxInterface):
     tiles_reader: dict
+    tile_positions: dict
     canvas: np.ndarray
 
     def __init__(self, seen_tiles: dict[str, Path], ctx: Context, gray=True):
@@ -26,6 +27,8 @@ class MountFrame(CtxInterface):
                                      vp_res='1320x1080',
                                      fov_res=self.fov)
         self.reset_readers()
+        self.clear_frame()
+        self.make_tile_positions()
 
     def reset_readers(self):
         self.tiles_reader = {}
@@ -40,22 +43,27 @@ class MountFrame(CtxInterface):
             self.canvas = np.zeros((proj_h, proj_w, 3), dtype='uint8')
 
     def get_frame(self):
-        self.clear_frame()
-
-        tile_h, tile_w = self.proj.tiling.tile_shape
-        tile_N, tile_M = self.proj.tiling.shape
+        self.canvas[:] = 0
 
         for tile in self.seen_tiles:
-            tile_m, tile_n = idx2xy(int(tile), (tile_N, tile_M))
-            tile_y, tile_x = tile_n * tile_h, tile_m * tile_w
-            y_ini = tile_y
-            y_end = tile_y + tile_h
-            x_ini = tile_x
-            x_end = tile_x + tile_w
-
+            x_ini, x_end, y_ini, y_end = self.tile_positions[tile]
             tile_frame = next(self.tiles_reader[tile])
             if self.gray:
                 self.canvas[y_ini:y_end, x_ini:x_end] = tile_frame
             else:
                 self.canvas[y_ini:y_end, x_ini:x_end, :] = tile_frame
         return self.canvas
+
+    def make_tile_positions(self):
+        self.tile_positions = {}
+        tile_h, tile_w = self.proj.tiling.tile_shape
+        tile_N, tile_M = self.proj.tiling.shape
+
+        for tile in self.tile_list:
+            tile_m, tile_n = idx2xy(int(tile), (tile_N, tile_M))
+            tile_y, tile_x = tile_n * tile_h, tile_m * tile_w
+            x_ini = tile_x
+            x_end = tile_x + tile_w
+            y_ini = tile_y
+            y_end = tile_y + tile_h
+            self.tile_positions[tile] = x_ini, x_end, y_ini, y_end
