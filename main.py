cc6ac352c48cb0bc15090a899e6e147849a3f441
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-01-13 16:49:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,18 @@ def main():
                                                                videos_list,
                                                                worker_list,
                                                                names_list))
-    parser.add_argument('-r', default=None, nargs=3, metavar=('CONFIG_ID', 'VIDEOS_LIST_ID', 'WORKER_ID'),
-                        help=f'Three string separated by space. See help text for details')
-    parser.add_argument('-slice', default=None, nargs=2, metavar=('VIDEO_START', 'VIDEO_STOP',),
-                        help=f'A int or slice of video range.', type=int)
-    parser.add_argument('-tiling', default=None, metavar=('TILING',),
+    parser.add_argument('-r', default=None, nargs=3, type=int,
+                        metavar=('CONFIG_ID', 'VIDEOS_LIST_ID', 'WORKER_ID'),
+                        help=f'Three string separated by space.')
+    parser.add_argument('-slice', default=None, nargs=2, type=int,
+                        metavar=('VIDEO_START', 'VIDEO_STOP',),
+                        help=f'A int or slice of video range.')
+    parser.add_argument('-tiling', default=None,
+                        metavar=('TILING',),
                         help=f'Force tiling.')
+    parser.add_argument('-quality', default=None,
+                        metavar=('QUALITY',),
+                        help=f'Force quality.')
     args = parser.parse_args()
 
     if args.r is None:
@@ -42,30 +48,33 @@ def main():
         config_file = menu(config_list, init_indent=1).obj
 
         print(f'Choose a videos list:')
-        videos_file = menu(videos_list, init_indent=1).obj
+        videos_opt = menu(videos_list, init_indent=1)
+        videos_file = videos_opt.obj
+        videos_list_id = videos_opt.id
 
         print(f'Choose a worker:')
         worker = menu(worker_list, init_indent=1).obj
     else:
-        config_id, videos_list_id, worker_id = map(int, args.r)
+        config_id, videos_list_id, worker_id = args.r
         config_file = get_option(config_id, config_list).obj
         videos_file = get_option(videos_list_id, videos_list).obj
         worker = get_option(worker_id, worker_list).obj
 
     config = Config(config_file, videos_file)
-    ctx = Context(config=config)
 
-    if (videos_file == get_option(0, videos_list).obj
-            and args.slice is not None):
+    if videos_list_id == 0 and args.slice is not None:
         start, stop = args.slice
         items_list = list(config.videos_dict.items())
         sliced_list = items_list[start:stop]
         config.videos_dict = dict(sliced_list)
 
-    if (videos_file == get_option(0, videos_list).obj
-            and args.tiling is not None):
+    if videos_list_id == 0 and args.tiling is not None:
         config.tiling_list = [args.tiling]
 
+    if videos_list_id == 0 and args.quality is not None:
+        config.quality_list = [args.quality]
+
+    ctx = Context(config=config)
     app: Worker = worker(ctx)
     print(f'\tTotal iterations = {app.ctx.iterations}')
 
