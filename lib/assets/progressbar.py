a6cdd0a4f81f340336fd5268ca1d00157fe40b84
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-02-23 15:59:43 -0300

    Create progressbar.py

---
+++ lib/assets/progressbar.py
@@ -0,0 +1,21 @@
+from tqdm import tqdm
+
+
+class ProgressBar:
+    t: tqdm
+
+    def __init__(self, total, desc):
+        self.t = tqdm(total=total, desc=desc, ncols=0)
+
+    def new(self, total, desc):
+        self.t = tqdm(total=total, desc=desc)
+
+    def update(self, postfix_str):
+        self.set_postfix_str(postfix_str)
+        self.t.update()
+
+    def set_postfix_str(self, postfix_str):
+        self.t.set_postfix_str(postfix_str)
+
+    def __del__(self):
+        self.t.close()
