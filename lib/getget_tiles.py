2f056ad45d7e39af27fb1aacb435b544aafb7160
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-02-23 16:15:09 -0300

    Update getget_tiles.py

--- lib/getget_tiles.py
+++ lib/getget_tiles.py
@@ -4,7 +4,8 @@ from typing import Any
 from lib.assets.autodict import AutoDict
 from lib.assets.ctxinterface import CtxInterface
 from lib.assets.errors import AbortError
-from lib.assets.worker import Worker, ProgressBar
+from lib.assets.progressbar import ProgressBar
+from lib.assets.worker import Worker
 from lib.get_tiles import GetTilesPaths
 from lib.utils.util import print_error, save_json, load_json, get_nested_value
 
