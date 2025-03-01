ddd7088a44c8a9f40aa0171f020e2d56eb96179f
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-02-28 22:20:32 -0300

    Update makedecodable.py

--- lib/makedecodable.py
+++ lib/makedecodable.py
@@ -1,16 +1,15 @@
 from lib.assets.ctxinterface import CtxInterface
 from lib.assets.errors import AbortError
-from lib.assets.paths.make_decodable_paths import MakeDecodablePaths
+from lib.assets.paths.make_decodable_paths import MakeDecodablePaths, IfDecodablePaths
 from lib.assets.worker import Worker
 from lib.utils.context_utils import task
-from lib.utils.util import decode_video, run_command
+from lib.utils.util import run_command
 
 
-class MakeDecodable(Worker, CtxInterface):
+class MakeDecodable(Worker, IfDecodablePaths, CtxInterface):
     make_decodable_path: MakeDecodablePaths
     quality_list: list[str] = None
     decode_check = False
-    cmd: str
 
     def init(self):
         self.make_decodable_path = MakeDecodablePaths(self.ctx)
@@ -23,20 +22,19 @@ class MakeDecodable(Worker, CtxInterface):
                 self.work()
 
     def work(self):
-        self.check_decodable()
-        self.check_dash()
+        self.assert_decodable()
+        self.assert_dash()
         self.make_decodable_cmd()
         self.run()
 
-    def run(self):
-        run_command(self.cmd, folder=self.decodable_folder,
-                    ui_prefix='\t')
+    def assert_decodable(self):
+        try:
+            self._check_decodable()
+            raise AbortError(f'decodable_chunk is OK.')
+        except FileNotFoundError:
+            pass
 
-    def make_decodable_cmd(self):
-        self.cmd = (f'bash -c "cat {self.dash_init.as_posix()} {self.dash_m4s.as_posix()} '
-                    f'> {self.decodable_chunk.as_posix()}"')
-
-    def check_dash(self):
+    def assert_dash(self):
         msg = []
         if not self.dash_m4s.exists():
             msg += ['dash_m4s not exist']
@@ -47,42 +45,19 @@ class MakeDecodable(Worker, CtxInterface):
         if msg:
             raise AbortError('/'.join(msg))
 
-    def check_decodable(self):
-        try:
-            chunk_size = self.decodable_chunk.stat().st_size
-            if self.decode_check:
-                self.check_one_chunk_decode()
-        except FileNotFoundError:
-            return 'decodable_chunk not found. Continue.'
+    cmd: str
+
+    def make_decodable_cmd(self):
+        self.cmd = (f'bash -c "cat {self.dash_init.as_posix()} {self.dash_m4s.as_posix()} '
+                    f'> {self.decodable_chunk.as_posix()}"')
+
+    def run(self):
+        run_command(self.cmd, folder=self.decodable_folder,
+                    ui_prefix='\t')
+
+    def _check_decodable(self):
+        chunk_size = self.decodable_chunk.stat().st_size
         if chunk_size == 0:
             self.logger.register_log('Chunk size is 0.', self.decodable_chunk)
             self.decodable_chunk.unlink()
-            return 'decodable_chunk size 0. Cleaning and continue.'
-        raise AbortError(f'decodable_chunk is OK.')
-
-    def check_one_chunk_decode(self):
-        if self.status.get_status('decode_check_ok'):
-            raise AbortError(f'Decoding check is ok.')
-
-        print(f'\tDecoding check')
-        decodable_chunk = self.decodable_chunk
-        stdout = decode_video(decodable_chunk, ui_prefix='\t')
-        if "frame=   30" not in stdout:  # specific for ffmpeg 5.0
-            self.logger.register_log(f'Chunk Decode Error.', decodable_chunk)
-            raise FileNotFoundError(f'Chunk Decode Error.')
-
-    @property
-    def decodable_folder(self):
-        return self.make_decodable_path.decodable_folder
-
-    @property
-    def dash_init(self):
-        return self.make_decodable_path.dash_init
-
-    @property
-    def dash_m4s(self):
-        return self.make_decodable_path.dash_m4s
-
-    @property
-    def decodable_chunk(self):
-        return self.make_decodable_path.decodable_chunk
+            raise FileNotFoundError()
