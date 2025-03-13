4605dbf2e8b2f3919c3583f7a035db5652eddc32
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-03-13 09:34:56 -0300

    Update getdectime.py

--- lib/getdectime.py
+++ lib/getdectime.py
@@ -18,11 +18,9 @@ class GetDectime(Worker, CtxInterface):
     dectime_result: list
     dectime_paths: DectimePaths
     progress_bar: ProgressBar
-    status: AutoDict
 
     def __init__(self, ctx: Context):
         super().__init__(ctx)
-        self.status_file = None
 
     def iter_name_proj_tiling_tile_qlt_chunk(self):
         for self.name in self.name_list:
@@ -34,14 +32,14 @@ class GetDectime(Worker, CtxInterface):
                             yield
 
     def init(self):
-        self.status_file = Path(f'log/status_{self.__class__.__name__}.pickle')
         self.dectime_paths = DectimePaths(self.ctx)
         if self.dectime_paths.dectime_result_pickle.exists():
             print('file exists')
             exit(0)
 
         self.projection = 'cmp'
-        self.progress_bar = ProgressBar(total=(181
+        self.progress_bar = ProgressBar(total=(len(self.name_list)
+                                               * 181
                                                * len(self.quality_list)
                                                * len(self.chunk_list)),
                                         desc=f'{self.__class__.__name__}')
@@ -67,5 +65,4 @@ class GetDectime(Worker, CtxInterface):
             msg = f'Chunk is not decoded enough. {len(times)} times.'
             print_error(msg)
             self.logger.register_log(msg, self.dectime_paths.dectime_log)
-
         return np.average(times)
