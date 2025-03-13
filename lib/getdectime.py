b492d98d127f505ea23a3b900b294a285fb5fce3
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-03-13 12:54:21 -0300

    Update getdectime.py

--- lib/getdectime.py
+++ lib/getdectime.py
@@ -1,34 +1,30 @@
-from pathlib import Path
-from typing import Any
-
 import numpy as np
 import pandas as pd
 
-from lib.assets.autodict import AutoDict
 from lib.assets.context import Context
 from lib.assets.ctxinterface import CtxInterface
-from lib.assets.errors import AbortError
 from lib.assets.paths.dectimepaths import DectimePaths
 from lib.assets.progressbar import ProgressBar
 from lib.assets.worker import Worker
-from lib.utils.util import get_times, get_nested_value, save_pickle, load_pickle, print_error
+from lib.utils.util import get_times, save_pickle, print_error
 
 
 class GetDectime(Worker, CtxInterface):
-    dectime_result: list
     dectime_paths: DectimePaths
     progress_bar: ProgressBar
 
-    def __init__(self, ctx: Context):
-        super().__init__(ctx)
-
     def iter_name_proj_tiling_tile_qlt_chunk(self):
+        self.projection = 'cmp'
+        self.progress_bar = ProgressBar(total=(len(self.name_list)
+                                               * 181
+                                               ),
+                                        desc=f'{self.__class__.__name__}')
         for self.name in self.name_list:
             for self.tiling in self.tiling_list:
                 for self.tile in self.tile_list:
+                    self.progress_bar.update(f'{self.ctx}')
                     for self.quality in self.quality_list:
                         for self.chunk in self.chunk_list:
-                            self.progress_bar.update(f'{self.ctx}')
                             yield
 
     def init(self):
@@ -37,18 +33,11 @@ class GetDectime(Worker, CtxInterface):
             print('file exists')
             exit(0)
 
-        self.projection = 'cmp'
-        self.progress_bar = ProgressBar(total=(len(self.name_list)
-                                               * 181
-                                               * len(self.quality_list)
-                                               * len(self.chunk_list)),
-                                        desc=f'{self.__class__.__name__}')
-
     def main(self):
         dectime_result = []
         for _ in self.iter_name_proj_tiling_tile_qlt_chunk():
             dectime = self.get_dectime()
-            dectime_result.append((self.name, self.projection, self.tiling, int(self.tile), int(self.quality), int(self.chunk), dectime))
+            dectime_result.append((self.name, self.projection, self.tiling, int(self.tile), int(self.quality), int(self.chunk)-1, dectime))
 
         result = pd.DataFrame(dectime_result, columns=['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'dectime'])
         result.set_index(['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'], inplace=True)
