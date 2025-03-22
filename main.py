64b67f12d80e6debfee83f26c1d51903da95c344
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-03-22 17:53:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from lib.maketiles import MakeTiles
 from lib.makequality import TileQuality
 from lib.make_siti import MakeSiti
 from lib.getdectime import GetDectime
-from lib.getbitrate import GetBitrate
+from lib.get_bitrate import GetBitrate
 from lib.getquality import GetQuality
-from lib.getget_tiles import GetGetTiles
+from lib.get_seen_tiles import GetSeenTiles
 from lib.makeviewportquality import ViewportQuality
 from lib.utils.main_utils import make_help_txt, menu, Option, get_option
 
@@ -121,7 +121,7 @@ worker_list = [
     Option(id=7, name='GetBitrate', obj=GetBitrate),
     Option(id=8, name='GetDectime', obj=GetDectime),
     Option(id=9, name='GetQuality', obj=GetQuality),
-    Option(id=10, name='GetGetTiles', obj=GetGetTiles),
+    Option(id=10, name='GetSeenTiles', obj=GetSeenTiles),
     Option(id=11, name='ViewportQuality', obj=ViewportQuality),
 ]
 
