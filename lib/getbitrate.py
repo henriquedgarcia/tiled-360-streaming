23aaf4350432d9b418125dd7b44707074c70f6f0
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-03-21 21:15:35 -0300

    Update getbitrate.py

--- lib/getbitrate.py
+++ lib/getbitrate.py
@@ -1,13 +1,10 @@
 import pandas as pd
-from tqdm import tqdm
 
-from lib.assets.autodict import AutoDict
 from lib.assets.ctxinterface import CtxInterface
 from lib.assets.progressbar import ProgressBar
 from lib.assets.worker import Worker
 from lib.makedash import MakeDashPaths
-from lib.utils.context_utils import task
-from lib.utils.util import print_error, save_json, get_nested_value, save_pickle
+from lib.utils.util import save_pickle
 
 
 class GetBitrate(Worker, CtxInterface):
@@ -21,33 +18,51 @@ class GetBitrate(Worker, CtxInterface):
     decodable_paths: MakeDashPaths
     progress_bar: ProgressBar
 
-    def iter_name_proj_tiling_tile_qlt_chunk(self):
+    def iter_proj_tiling_tile_qlt_chunk(self):
         self.projection = 'cmp'
-        self.progress_bar = ProgressBar(total=(len(self.name_list)
+        self.progress_bar = ProgressBar(total=(len(self.quality_list)
                                                * 181
                                                ),
                                         desc=f'{self.__class__.__name__}')
-        for self.name in self.name_list:
-            for self.tiling in self.tiling_list:
-                for self.tile in self.tile_list:
-                    self.progress_bar.update(f'{self.ctx}')
-                    for self.quality in self.quality_list:
-                        for self.chunk in self.chunk_list:
-                            yield
+        for self.tiling in self.tiling_list:
+            for self.tile in self.tile_list:
+                self.progress_bar.update(f'{self.ctx}')
+                for self.quality in self.quality_list:
+                    for self.chunk in self.chunk_list:
+                        yield
 
     def init(self):
         self.decodable_paths = MakeDashPaths(self.ctx)
-        if self.decodable_paths.bitrate_result_pickle.exists():
-            print('file exists')
-            exit(0)
 
     def main(self):
-        bitrate_result = []
-        for _ in self.iter_name_proj_tiling_tile_qlt_chunk():
-            bitrate = self.decodable_paths.dash_m4s.stat().st_size * 8
-            bitrate_result.append((self.name, self.projection, self.tiling, int(self.tile), int(self.quality), int(self.chunk), bitrate))
+        for self.name in self.name_list:
+            if self.decodable_paths.bitrate_result_pickle.exists():
+                print('file exists')
+                continue
 
-        result = pd.DataFrame(bitrate_result, columns=['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'bitrate'])
-        result.set_index(['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'], inplace=True)
-        save_pickle(result['bitrate'], self.decodable_paths.bitrate_result_pickle)
-        print('finished')
+            bitrate_result = []
+            for _ in self.iter_proj_tiling_tile_qlt_chunk():
+                bitrate = self.get_bitrate()
+                self.set_bitrate(bitrate_result, bitrate)
+
+            result = pd.DataFrame(bitrate_result,
+                                  columns=['name', 'projection',
+                                           'tiling', 'tile',
+                                           'quality', 'chunk',
+                                           'bitrate'])
+            result.set_index(['name', 'projection', 'tiling',
+                              'tile', 'quality', 'chunk'],
+                             inplace=True)
+            save_pickle(result['bitrate'],
+                        self.decodable_paths.bitrate_result_pickle)
+            print('finished')
+
+    def set_bitrate(self, bitrate_result, bitrate):
+        key = [self.name, self.projection, self.tiling,
+               int(self.tile), int(self.quality),
+               int(self.chunk) - 1, bitrate]
+        bitrate_result.append(key)
+
+    def get_bitrate(self):
+        chunk_size = self.decodable_paths.dash_m4s.stat().st_size
+        return chunk_size * 8
