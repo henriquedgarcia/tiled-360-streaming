805d839be6b1203edafe2afd06fd8c8563633e34
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-03-25 17:15:37 -0300

    Update makesitipaths.py

--- lib/assets/paths/makesitipaths.py
+++ lib/assets/paths/makesitipaths.py
@@ -21,21 +21,27 @@ class MakeSitiPaths(CtxInterface):
         return self.siti_folder / f'siti_stats.csv'
 
     @property
-    def siti_plot(self) -> Path:
-        return self.siti_folder / f'siti_plot.png'
+    def siti_all_plot(self) -> Path:
+        return self.siti_folder / f'siti_plot_all.png'
 
     @property
-    def siti_results(self) -> Path:
-        name = f'siti_results'
+    def siti_name_plot(self) -> Path:
+        folder = self.siti_folder / 'plot'
+        folder.mkdir(exist_ok=True, parents=True)
+        return folder / f'siti_plot_{self.name}.png'
 
+    @property
+    def siti_csv_results(self) -> Path:
+        folder = self.siti_folder / 'siti_csv'
+        folder.mkdir(exist_ok=True, parents=True)
+        name = ''
         if self.name is not None:
-            name += f'_{self.name}'
+            name += f'{self.name}'
         if self.tiling is not None:
             name += f'_{self.tiling}'
-        if self.quality is not None:
-            name += f'_{self.rate_control}{self.quality}'
         if self.tile is not None:
             name += f'_tile{self.tile}'
-        if self.chunk is not None:
-            name += f'_chunk{self.chunk}'
-        return self.siti_folder / f"siti/{self.name}.csv"
+        if self.quality is not None:
+            name += f'{self.rate_control}{self.quality}'
+
+        return folder / f"{name}.csv"
