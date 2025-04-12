ca35b2b5e6a8a0f84ef1e913856fc46b4f9d0f93
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-04-12 12:38:14 -0300

    Update get_dectime.py

--- lib/get_dectime.py
+++ lib/get_dectime.py
@@ -1,23 +1,22 @@
 import numpy as np
 import pandas as pd
 
-from lib.assets.ctxinterface import CtxInterface
 from lib.assets.errors import AbortError
 from lib.assets.paths.dectimepaths import DectimePaths
 from lib.assets.progressbar import ProgressBar
 from lib.assets.worker import Worker
-from lib.utils.util import get_times, save_pickle, print_error
+from lib.utils.util import get_times, print_error
 
 
-class GetDectime(Worker, CtxInterface):
+class GetDectime(Worker, DectimePaths):
     dectime_paths: DectimePaths
     progress_bar: ProgressBar
 
-    def iter_proj_tiling_tile_qlt_chunk(self):
+    def init(self):
         self.projection = 'cmp'
-        self.progress_bar = ProgressBar(total=(len(self.quality_list)
-                                               * 181
-                                               ),
+
+    def iter_proj_tiling_tile_qlt_chunk(self):
+        self.progress_bar = ProgressBar(total=(len(self.quality_list) * 181),
                                         desc=f'{self.__class__.__name__}')
         for self.tiling in self.tiling_list:
             for self.tile in self.tile_list:
@@ -27,29 +26,27 @@ class GetDectime(Worker, CtxInterface):
                         yield
                     self.chunk = None
 
-    def init(self):
-        self.dectime_paths = DectimePaths(self.ctx)
-        if self.dectime_paths.dectime_result_pickle.exists():
-            print_error('file exists')
-            exit(0)
-
     def main(self):
+        columns = ['name', 'projection', 'tiling', 'tile',
+                   'quality', 'chunk', 'dectime']
+
         for self.name in self.name_list:
             if self.dectime_paths.dectime_result_pickle.exists():
                 print_error(f'{self.dectime_paths.dectime_result_pickle} exists')
                 continue
 
             dectime_result = []
-            for _ in self.iter_proj_tiling_tile_qlt_chunk():
-                dectime = self.get_dectime()
-                self.set_dectime(dectime_result, dectime)
+            for self.tiling in self.tiling_list:
+                for self.tile in self.tile_list:
+                    for self.quality in self.quality_list:
+                        self.progress_bar.update(f'{self.ctx}')
+                        for self.chunk in self.chunk_list:
+                            dectime = self.get_dectime()
+                            self.set_dectime(dectime_result, dectime)
 
-            result = pd.DataFrame(dectime_result,
-                                  columns=['name', 'projection', 'tiling', 'tile',
-                                           'quality', 'chunk', 'dectime'])
-            result.set_index(['name', 'projection', 'tiling', 'tile',
-                              'quality', 'chunk'], inplace=True)
-            save_pickle(result['dectime'], self.dectime_paths.dectime_result_pickle)
+            result = pd.DataFrame(dectime_result, columns=columns)
+            result.set_index(columns[:-1], inplace=True)
+            result['dectime'].to_pickle(self.dectime_paths.dectime_result_pickle)
             print('finished')
 
     def set_dectime(self, dectime_result, dectime):
