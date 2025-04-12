802090e37bea5e02b16917e00028a0ebd192425e
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-04-12 12:38:20 -0300

    Update dectimepaths.py

--- lib/assets/paths/dectimepaths.py
+++ lib/assets/paths/dectimepaths.py
@@ -1,15 +1,9 @@
 from pathlib import Path
 
-from lib.assets.context import Context
-from lib.assets.paths.basepaths import BasePaths
 from lib.assets.paths.make_decodable_paths import MakeDecodablePaths
 
 
-class DectimePaths(BasePaths):
-    def __init__(self, context: Context):
-        self.ctx = context
-        self.decodable_paths = MakeDecodablePaths(context)
-
+class DectimePaths(MakeDecodablePaths):
     @property
     def dectime_folder(self) -> Path:
         return self.dectime_folder / self.folder_name_proj_tiling_tile_qlt
@@ -18,10 +12,6 @@ class DectimePaths(BasePaths):
     def dectime_log(self) -> Path:
         chunk = int(self.ctx.chunk)
         return self.dectime_folder / f'chunk{chunk:03d}_dectime.log'
-
-    @property
-    def decodable_chunk(self) -> Path:
-        return self.decodable_paths.decodable_chunk
 
     @property
     def dectime_result_json(self) -> Path:
