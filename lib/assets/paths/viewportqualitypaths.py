2852a00a91e59786151ff07878af3bac394b5109
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-04-12 12:09:44 -0300

    Update viewportqualitypaths.py

--- lib/assets/paths/viewportqualitypaths.py
+++ lib/assets/paths/viewportqualitypaths.py
@@ -20,3 +20,12 @@ class ViewportQualityPaths(BasePaths):
         """
 
         return self.results_folder / f'user_viewport_quality/user_viewport_quality_{self.name}.json'
+
+    @property
+    def user_viewport_quality_result_pickle(self) -> Path:
+        """
+        Need name tiling user quality chunk
+        """
+        folder = self.results_folder / f'user_viewport_quality'
+        folder.mkdir(parents=True, exist_ok=True)
+        return folder / f'user_viewport_quality_{self.name}.pickle'
