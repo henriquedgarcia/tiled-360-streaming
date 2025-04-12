625bd6c965ee347c0b79fd5be75304c9d41069fa
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-04-12 12:38:26 -0300

    Update makedashpaths.py

--- lib/assets/paths/makedashpaths.py
+++ lib/assets/paths/makedashpaths.py
@@ -1,19 +1,9 @@
 from pathlib import Path
 
-from lib.assets.context import Context
-from lib.assets.paths.basepaths import BasePaths
 from lib.assets.paths.maketilespaths import MakeTilesPaths
 
 
-class MakeDashPaths(BasePaths):
-    def __init__(self, context: Context):
-        self.ctx = context
-        self.make_tiles_paths = MakeTilesPaths(context)
-
-    @property
-    def tile_video(self) -> Path:
-        return self.make_tiles_paths.tile_video
-
+class MakeDashPaths(MakeTilesPaths):
     @property
     def mp4box_log(self) -> Path:
         return self.dash_folder / self.folder_name_proj_tiling / f'tile{self.tile}_segmenter.log'
@@ -27,12 +17,12 @@ class MakeDashPaths(BasePaths):
         return self.mpd_folder / f'tile{self.tile}.mpd'
 
     @property
-    def dash_m4s(self) -> Path:
-        return self.mpd_folder / f'tile{self.tile}_{self.rate_control}{self.quality}_{self.chunk}.m4s'
-
-    @property
     def dash_init(self) -> Path:
         return self.mpd_folder / f'tile{self.tile}_{self.rate_control}{self.quality}_init.mp4'
+
+    @property
+    def dash_m4s(self) -> Path:
+        return self.mpd_folder / f'tile{self.tile}_{self.rate_control}{self.quality}_{self.chunk}.m4s'
 
     @property
     def bitrate_result_json(self) -> Path:
