b7a06ca0d9434eb2cb94d8f6dc4c129654454883
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-05-16 16:57:52 -0300

    Update get_dectime.py

--- lib/get_dectime.py
+++ lib/get_dectime.py
@@ -1,6 +1,11 @@
+import os
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
+from config.config import Config
+from lib.assets.context import Context
 from lib.assets.errors import AbortError
 from lib.assets.paths.dectimepaths import DectimePaths
 from lib.assets.worker import Worker
@@ -8,40 +13,66 @@ from lib.utils.util import get_times, print_error
 
 
 class GetDectime(Worker, DectimePaths):
-    def init(self):
-        self.projection = 'cmp'
-        self.index_names = ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
-        self.cools_names = ['dectime']
-
-    index_names: list
-    cools_names: list
     data: list
+    cools_names: list
+
+    def init(self):
+        self.cools_names = ['name', 'projection', 'tiling', 'tile', 'quality',
+                            'chunk', 'dectime']
 
     def main(self):
+        for _ in self.iterate_name_projection:
+            if self.dectime_result_by_name.exists(): continue
+
+            try:
+                self.get_data()
+            except FileNotFoundError:
+                print(f'\nFileNotFoundError: {self.dectime_log} not found.')
+                self.logger.register_log('FileNotFoundError', self.dectime_log)
+                continue
+
+            print('\tSaving Data')
+            self.save_data()
+            print('\tfinished')
+        self.merge()
+
+    def merge(self):
+        if self.dectime_result.exists():
+            print('dectime_result is OK.')
+            return
+
+        merged = None
+
+        for _ in self.iterate_name_projection:
+            df = pd.read_pickle(self.dectime_result_by_name)
+            merged = (df if merged is None
+                      else pd.concat([merged, df], axis=0))
+
+        if merged.size != 434400:
+            print_error('Dataframe size mismatch.')
+            raise AbortError
+
+        merged.to_pickle(self.dectime_result)
+
+    def __str__(self):
+        return f'{self.name}_{self.projection}_{self.tiling}_tile{self.tile}_{self.rate_control}{self.quality}_chunk{self.chunk}'
+
+    def get_data(self):
         self.data = []
-        total = 28 * 181
-        n = iter(range(total))
-        for self.name in self.name_list:
-            for self.tiling in self.tiling_list:
-                for self.tile in self.tile_list:
-                    print(f'\r{next(n)}/{total} - {self.__class__.__name__} - {self.name}_{self.tiling}_tile{self.tile}                   ', end='')
-                    for self.quality in self.quality_list:
-                        for self.chunk in self.chunk_list:
-                            dectime = self.get_dectime()
-                            key = (self.name, self.projection, self.tiling,
-                                   int(self.tile), int(self.quality),
-                                   int(self.chunk) - 1, dectime)
-                            self.data.append(key)
+        for _ in self.iterate_tiling_tile_quality_chunk:
+            print(f'\r{self}', end='')
 
-        print('\nSaving Pickle')
-        self.save_pickle(self.dectime_result_pickle)
-        print('finished')
+            dectime = self.get_dectime()
+            key = (self.name, self.projection, self.tiling,
+                   int(self.tile), int(self.quality),
+                   int(self.chunk) - 1, dectime)
+            self.data.append(key)
 
-    def save_pickle(self, filename):
-        df = pd.DataFrame(self.data, columns=self.index_names + self.cools_names)
-        df.set_index(self.index_names[:-1], inplace=True)
+    def save_data(self):
+        df = pd.DataFrame(self.data, columns=self.cools_names)
+        df.set_index(self.cools_names[:-1], inplace=True)
         df.sort_index(inplace=True)
-        pd.to_pickle(df, filename)
+        pd.to_pickle(df, self.dectime_result_by_name)
 
     def get_dectime(self):
         try:
@@ -51,7 +82,31 @@ class GetDectime(Worker, DectimePaths):
 
         if len(times) < self.config.decoding_num:
             msg = f'Chunk is not decoded enough. {len(times)} times.'
-            print_error(msg)
             self.logger.register_log(msg, self.dectime_log)
             raise AbortError(msg)
         return np.average(times)
+
+
+if __name__ == '__main__':
+    os.chdir('../')
+
+    # config_file = 'config_erp_qp.json'
+    # config_file = 'config_cmp_crf.json'
+    # config_file = 'config_erp_crf.json'
+    # videos_file = 'videos_reversed.json'
+    # videos_file = 'videos_lumine.json'
+    # videos_file = 'videos_container0.json'
+    # videos_file = 'videos_container1.json'
+    # videos_file = 'videos_fortrek.json'
+    # videos_file = 'videos_hp_elite.json'
+    # videos_file = 'videos_alambique.json'
+    # videos_file = 'videos_test.json'
+    # videos_file = 'videos_full.json'
+
+    config_file = Path('config/config_cmp_qp.json')
+    videos_file = Path('config/videos_reduced.json')
+
+    config = Config(config_file, videos_file)
+    ctx = Context(config=config)
+
+    GetDectime(ctx)
