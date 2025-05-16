805ea9788c5d24f70bfdf48f327d6514967f9d5f
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-05-16 16:57:38 -0300

    Update viewportqualitypaths.py

--- lib/assets/paths/viewportqualitypaths.py
+++ lib/assets/paths/viewportqualitypaths.py
@@ -5,27 +5,25 @@ from lib.assets.paths.basepaths import BasePaths
 
 class ViewportQualityPaths(BasePaths):
     @property
+    def user_viewport_folder(self):
+        folder = (self.viewport_quality_folder /
+                  f'{self.name}' / f'{self.tiling}' / f'{self.user}' / f'{self.quality}')
+        folder.mkdir(parents=True, exist_ok=True)
+        return folder
+
+    @property
     def user_viewport_quality_json(self) -> Path:
         """
         Need name tiling user quality chunk
         """
-        return (self.viewport_quality_folder /
-                f'{self.name}' / f'{self.tiling}' / f'{self.user}' / f'{self.quality}' /
-                f'user_viewport_quality_chunk{self.chunk}.json')
+        filename = f'user_viewport_quality_chunk{self.chunk}.json'
+        return self.user_viewport_folder / filename
 
     @property
-    def user_viewport_quality_result_json(self) -> Path:
-        """
-        Need name tiling user quality chunk
-        """
-
-        return self.results_folder / f'user_viewport_quality/user_viewport_quality_{self.name}.json'
+    def user_viewport_result_by_name(self) -> Path:
+        return self.viewport_quality_folder / f'user_viewport_quality_{self.metric}_{self.name}_{self.projection}_{self.rate_control}.pickle'
 
     @property
-    def user_viewport_quality_result_pickle(self) -> Path:
-        """
-        Need name tiling user quality chunk
-        """
-        folder = self.results_folder / f'user_viewport_quality'
-        folder.mkdir(parents=True, exist_ok=True)
-        return folder / f'user_viewport_quality_{self.name}.pickle'
+    def chunk_quality_result(self) -> Path:
+        """depend on name and fov"""
+        return self.results_folder / f'chunk_quality_{self.metric}_{self.projection}_{self.rate_control}.pickle'
