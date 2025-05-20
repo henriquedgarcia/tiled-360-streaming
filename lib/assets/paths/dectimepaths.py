7b329405de426861b91811f74a75d2974ef2ff65
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-05-20 17:41:49 -0300

    Update dectimepaths.py

--- lib/assets/paths/dectimepaths.py
+++ lib/assets/paths/dectimepaths.py
@@ -5,21 +5,21 @@ from lib.assets.paths.make_decodable_paths import MakeDecodablePaths
 
 class DectimePaths(MakeDecodablePaths):
     @property
-    def dectime_folder(self) -> Path:
-        return self.dectime_folder0 / self.folder_name_proj_tiling_tile_qlt
+    def chunk_dectime_folder(self) -> Path:
+        return self.dectime_folder / self.folder_name_proj_tiling_tile_qlt
 
     @property
     def dectime_log(self) -> Path:
         chunk = int(self.chunk)
-        return self.dectime_folder / f'chunk{chunk:03d}_dectime.log'
-
-    @property
-    def dectime_result_json(self) -> Path:
-        return self.results_folder / f'dectime/time_{self.ctx.name}.json'
+        return self.chunk_dectime_folder / f'chunk{chunk:03d}_dectime.log'
 
     @property
     def dectime_result_by_name(self) -> Path:
-        return self.dectime_folder0 / f'dectime_{self.name}_{self.projection}_{self.rate_control}.pickle'
+        return self.dectime_folder / f'dectime_{self.name}_{self.projection}_{self.rate_control}.pickle'
+
+    @property
+    def dectime_result_deprecated(self) -> Path:
+        return self.results_folder / f'dectime/time_{self.ctx.name}.json'
 
     @property
     def dectime_result(self) -> Path:
