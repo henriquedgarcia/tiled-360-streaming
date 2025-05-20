a9154ea592d499ff3803b2313836bd6dfd1a83b0
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-05-20 18:45:20 -0300

    Update get_bitrate.py

--- lib/get_bitrate.py
+++ lib/get_bitrate.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     # videos_file = 'videos_test.json'
     # videos_file = 'videos_full.json'
 
-    config_file = Path('config/config_cmp_qp.json')
+    config_file = Path('config/config_erp_qp.json')
     videos_file = Path('config/videos_reduced.json')
 
     config = Config(config_file, videos_file)
