2b737cca64dedfc7927ca001be588f5a25f32a60
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-06-17 18:43:37 -0300

    Update get_bitrate.py

--- lib/get_bitrate.py
+++ lib/get_bitrate.py
@@ -8,6 +8,7 @@ from lib.assets.context import Context
 from lib.assets.errors import AbortError
 from lib.assets.worker import Worker
 from lib.make_dash import MakeDashPaths
+from lib.utils.context_utils import task
 from lib.utils.util import print_error
 
 
@@ -19,51 +20,48 @@ class GetBitrate(Worker, MakeDashPaths):
                                   ['dash_m4s'][self.quality][self.chunk]
     :return:
     """
-    data: list
     cools_names: list
+    total_by_name: int
 
     def init(self):
         self.cools_names = ['name', 'projection', 'tiling', 'tile', 'quality',
                             'chunk', 'bitrate']
+        self.total_by_name = 181 * len(self.quality_list) * len(self.chunk_list)
 
     def main(self):
         for _ in self.iterate_name_projection:
-            if self.bitrate_result_by_name.exists(): continue
-
-            try:
+            with task(self):
+                self.check_bitrate_result_by_name()
                 self.get_data()
-            except FileNotFoundError:
-                print(f'\nFileNotFoundError: {self.dash_m4s} not found.')
-                self.logger.register_log('FileNotFoundError', self.dash_m4s)
-                continue
-
-            print('\tSaving Data')
-            self.save_data()
-            print('\tfinished')
 
         self.merge()
 
-    def __str__(self):
-        return f'{self.name}_{self.projection}_{self.tiling}_{self.tile}_{self.quality}'
+    def check_bitrate_result_by_name(self):
+        try:
+            df = pd.read_pickle(self.bitrate_result_by_name)
+            if df.size == self.total_by_name:
+                raise AbortError('bitrate_result_by_name is OK.')
+        except FileNotFoundError:
+            pass
 
     def get_data(self):
-        self.data = []
-        for _ in self.iterate_tiling_tile_quality_chunk:
-            if self.quality == self.quality_list[0]: print(str(self))
+        data = []
+        for n in self.iterate_tiling_tile_quality_chunk:
+            print(f'\n\rProcessing {n}/{self.total_by_name} - {self.ctx}', end='')
             bitrate = self.dash_m4s.stat().st_size * 8
 
             key = (self.name, self.projection, self.tiling,
                    int(self.tile), int(self.quality),
                    int(self.chunk) - 1, bitrate)
-            self.data.append(key)
+            data.append(key)
+
+        print('\nSaving')
+        df = pd.DataFrame(data, columns=self.cools_names)
+        df.set_index(self.cools_names[:-1], inplace=True)
+        pd.to_pickle(df, self.bitrate_result_by_name)
 
     def merge(self):
-        if self.bitrate_result.exists():
-            print('bitrate_result is OK.')
-            return
-
         merged = None
-
         for _ in self.iterate_name_projection:
             df = pd.read_pickle(self.bitrate_result_by_name)
             merged = (df if merged is None
@@ -73,32 +71,14 @@ class GetBitrate(Worker, MakeDashPaths):
             print_error('Dataframe size mismatch.')
             raise AbortError
 
-        merged.to_pickle(self.bitrate_result)
-
-    def save_data(self):
-        df = pd.DataFrame(self.data, columns=self.cools_names)
-        df.set_index(self.cools_names[:-1], inplace=True)
-        df.sort_index(inplace=True)
-        pd.to_pickle(df, self.bitrate_result_by_name)
+        merged.to_pickle(self.bitrate_result)  # self.results_folder / f'bitrate_{self.projection}_{self.rate_control}.pickle'
 
 
 if __name__ == '__main__':
     os.chdir('../')
 
-    # config_file = 'config_erp_qp.json'
-    # config_file = 'config_cmp_crf.json'
-    # config_file = 'config_erp_crf.json'
-    # videos_file = 'videos_reversed.json'
-    # videos_file = 'videos_lumine.json'
-    # videos_file = 'videos_container0.json'
-    # videos_file = 'videos_container1.json'
-    # videos_file = 'videos_fortrek.json'
-    # videos_file = 'videos_hp_elite.json'
-    # videos_file = 'videos_alambique.json'
-    # videos_file = 'videos_test.json'
-    # videos_file = 'videos_full.json'
-
-    config_file = Path('config/config_erp_qp.json')
+    # config_file = Path('config/config_erp_qp.json')
+    config_file = Path('config/config_cmp_qp.json')
     videos_file = Path('config/videos_reduced.json')
 
     config = Config(config_file, videos_file)
