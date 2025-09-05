a949c34a91a98798d79db6f16c303d18cc9d5e8c
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-09-05 13:40:15 -0300

    Update make_tiles.py

--- lib/make_tiles.py
+++ lib/make_tiles.py
@@ -1,24 +1,35 @@
 import os
 from pathlib import Path
 
+from py360tools import ERP, CMP, ProjectionBase
+
 from config.config import Config
 from lib.assets.context import Context
-from lib.assets.errors import AbortError
+from lib.assets.errors import AbortError, TilesOkError
 from lib.assets.paths.maketilespaths import MakeTilesPaths
 from lib.assets.worker import Worker
 from lib.utils.context_utils import task
-from lib.utils.util import run_command
+from lib.utils.util import run_command, get_tile_position
 
 
 class MakeTiles(Worker, MakeTilesPaths):
-    quality_list: list[str] = None
-    decode_check = False
+    tile_position: tuple[int, int, int, int]
+    proj_obj: ProjectionBase
 
-    def init(self):
-        self.quality_list = ['0'] + self.ctx.quality_list
+    @property
+    def iterate_name_projection_tiling_tile_quality(self):
+        for self.name in self.name_list:
+            for self.projection in self.projection_list:
+                for self.tiling in self.tiling_list:
+                    self.proj_obj = (ERP if self.projection == 'erp' else CMP)(proj_res=self.proj_res, tiling=self.tiling)
+                    for self.tile in self.tile_list:
+                        tile = self.proj_obj.tile_list[int(self.tile)]
+                        self.tile_position = get_tile_position(tile)
+                        for self.quality in self.quality_list:
+                            self.ctx.iterations += 1
+                            yield
 
     def main(self):
-        self.init()
         for _ in self.iterate_name_projection_tiling_tile_quality:
             with task(self):
                 self.make_tile()
@@ -28,17 +39,16 @@ class MakeTiles(Worker, MakeTilesPaths):
         self.assert_lossless()
 
         cmd = self.make_tile_cmd()
-        run_command(cmd=cmd,
-                    folder=self.tile_folder,
-                    log_file=self.tile_log,
+        run_command(cmd=cmd, folder=self.tile_folder, log_file=self.tile_log,
                     ui_prefix='\t')
 
     def assert_tiles(self):
         try:
             self.check_tile_video()
-            raise AbortError('')
+            raise TilesOkError('')
         except FileNotFoundError:
-            self.clean_tile()
+            self.tile_log.unlink(missing_ok=True)
+            self.tile_video.unlink(missing_ok=True)
 
     def check_tile_video(self):
         compressed_file_size = self.tile_video.stat().st_size
@@ -57,69 +67,44 @@ class MakeTiles(Worker, MakeTilesPaths):
 
     def assert_lossless(self):
         try:
-            self.check_lossless()
+            lossless_video_size = self.lossless_video.stat().st_size
+            if lossless_video_size == 0:
+                raise FileNotFoundError('lossless_video not found.')
+
         except FileNotFoundError:
             self.logger.register_log('lossless_video not found.', self.lossless_video)
             raise AbortError(f'lossless_video not found.')
 
     def make_tile_cmd(self) -> str:
-        lossless_file = self.lossless_video.as_posix()
-        compressed_file = self.tile_video.as_posix()
-
-        x1, y1, x2, y2 = self.tile_position_dict[self.scale][self.tiling][self.tile]
+        x1, x2, y1, y2 = self.tile_position
+        crop_params = f'crop=w={x2 - x1}:h={y2 - y1}:x={x1}:y={y1}'
 
         gop_options = f'keyint={self.gop}:min-keyint={self.gop}:open-gop=0'
-        misc_options = f'scenecut=0:info=0'
+        misc_options = f':scenecut=0:info=0'
         qp_options = ':ipratio=1:pbratio=1' if self.rate_control == 'qp' else ''
         lossless_option = ':lossless=1' if self.quality == '0' else ''
-        codec_params = f'-x265-params {gop_options}:{misc_options}{qp_options}{lossless_option}'
+        codec_params = f'-x265-params {gop_options}{misc_options}{qp_options}{lossless_option}'
         codec = f'-c:v libx265'
-        crop_params = f'crop=w={x2 - x1}:h={y2 - y1}:x={x1}:y={y1}'
         output_options = f'-{self.rate_control} {self.quality} -tune psnr'
 
         cmd = ('bash -c '
                '"'
                'bin/ffmpeg -hide_banner -y -psnr '
-               f'-i {lossless_file} '
+               f'-i {self.lossless_video.as_posix()} '
                f'{output_options} '
                f'{codec} {codec_params} '
                f'-vf {crop_params} '
-               f'{compressed_file}'
+               f'{self.tile_video.as_posix()}'
                f'"')
 
         return cmd
 
-    def check_lossless(self):
-        lossless_video_size = self.lossless_video.stat().st_size
-        if lossless_video_size == 0:
-            raise FileNotFoundError('lossless_video not found.')
-
-    def clean_tile(self):
-        self.tile_log.unlink(missing_ok=True)
-        self.tile_video.unlink(missing_ok=True)
-
 
 if __name__ == '__main__':
     os.chdir('../')
-
-    # config_file = 'config_cmp_crf.json'
-    # config_file = 'config_erp_crf.json'
-    # videos_file = 'videos_reversed.json'
-    # videos_file = 'videos_lumine.json'
-    # videos_file = 'videos_container0.json'
-    # videos_file = 'videos_container1.json'
-    # videos_file = 'videos_fortrek.json'
-    # videos_file = 'videos_hp_elite.json'
-    # videos_file = 'videos_alambique.json'
-    # videos_file = 'videos_test.json'
-    # videos_file = 'videos_full.json'
-
-    config_file = Path('config/config_cmp_crf.json')
-    # config_file = Path('config/config_erp_qp.json')
-    # config_file = Path('config/config_cmp_qp.json')
-    videos_file = Path('config/videos_reduced.json')
+    config_file = Path('config/config_pres_qp.json')
+    videos_file = Path('config/videos_pres.json')
 
     config = Config(config_file, videos_file)
     ctx = Context(config=config)
-
-    MakeTiles(ctx)
+    MakeTiles(ctx).run()
