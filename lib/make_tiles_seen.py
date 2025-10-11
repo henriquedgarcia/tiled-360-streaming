f5f52ebf0a0fe5e8dd059869add393ac16087751
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-10-10 21:43:17 -0300

    Update make_tiles_seen.py

--- lib/make_tiles_seen.py
+++ lib/make_tiles_seen.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
-from typing import Optional, Any
+from typing import Optional, Any, Union
 
-from py360tools import ProjectionBase, Viewport, ERP, CMP, Tile
+from py360tools import Viewport, Tile, ProjectionBase, ERP, CMP
 
 from config.config import Config
 from lib.assets.autodict import AutoDict
@@ -15,7 +15,7 @@ from lib.utils.io_util import save_json
 from lib.utils.util import get_nested_value
 
 
-class PrivatesMethods(TilesSeenPaths):
+class PrivatesMethods():
     _results: dict = None
 
     @property
@@ -70,30 +70,16 @@ class PrivatesMethods(TilesSeenPaths):
         return tiles_seen_by_chunk
 
 
-class MakeTilesSeen(Worker, PrivatesMethods):
-    viewport_dict: dict['str', dict['str', ProjectionBase]]
+class MakeTilesSeen(Worker, TilesSeenPaths):
+    viewport_dict: dict['str', dict['str', Viewport]]
     tiles_seen: dict
 
     def init(self):
-        def create_viewport_dict():
-            """
-            viewport_dict = {'cmp': {'3x2': Viewport(vp_res, self.fov, CMP(tiling='3x2', proj_res=proj_res)),
-                                     '6x4': Viewport(vp_res, self.fov, CMP(tiling='6x4', proj_res=proj_res)),
-                                     ...},
-                             'erp': {'3x2': Viewport(vp_res, self.fov, ERP(tiling='3x2', proj_res=proj_res)),
-                                     '6x4': Viewport(vp_res, self.fov, ERP(tiling='6x4', proj_res=proj_res)),
-                                     ...}
-                             }
-            :return:
-            """
-            viewport_dict = AutoDict()
-            for tiling in self.tiling_list:
-                for self.projection in self.projection_list:
-                    proj = eval(self.projection.upper())
-                    viewport_dict[self.projection][tiling] = Viewport(self.vp_res, self.fov, proj(tiling=tiling, proj_res=self.scale))
-            return viewport_dict
-
-        self.viewport_dict = create_viewport_dict()
+        self.viewport_dict = AutoDict()
+        for tiling in self.tiling_list:
+            for self.projection in self.projection_list:
+                proj: type(ProjectionBase) = eval(self.projection.upper())
+                self.viewport_dict[self.projection][tiling] = Viewport(self.vp_res, self.fov, proj(tiling=tiling, proj_res=self.scale))
 
     def main(self):
         for _ in self.iterate_name_projection_tiling_user():
@@ -101,7 +87,6 @@ class MakeTilesSeen(Worker, PrivatesMethods):
                 self.check_seen_tiles()
                 self.check_user_hmd_data()
                 self.get_tiles_seen()
-                self.save_tiles_seen()
 
     def check_seen_tiles(self):
         try:
@@ -116,7 +101,7 @@ class MakeTilesSeen(Worker, PrivatesMethods):
         raise AbortError('Seen tiles JSON is OK.')
 
     def check_user_hmd_data(self):
-        if self.user_hmd_data == {}:
+        if self.user_hmd_data is None:
             self.logger.register_log(f'HMD samples is missing, '
                                      f'user{self.user}',
                                      self.config.dataset_file)
@@ -126,13 +111,18 @@ class MakeTilesSeen(Worker, PrivatesMethods):
     def get_tiles_seen(self):
         print('')
         with timer(ident=1):
-            tiles_seen_by_frame = self.get_tiles_seen_by_frame()
+            if self.tiling == '1x1': return [["0"]] * self.n_frames
+
+            tiles_seen_by_frame = []
+            viewport_obj: Viewport = self.viewport_dict[self.projection][self.tiling]
+
+            for frame, yaw_pitch_roll in enumerate(self.user_hmd_data, 1):
+                print(f'\r\tframe {frame:04d}/{self.n_frames}', end='')
+                vptiles: list[Tile] = viewport_obj.get_vptiles(yaw_pitch_roll)
+                vptiles: list[str] = [str(tile.idx) for tile in vptiles]
+                tiles_seen_by_frame.append(vptiles)
             self.tiles_seen = {'frames': tiles_seen_by_frame}
-
-    def save_tiles_seen(self):
-        save_json(self.tiles_seen, self.user_seen_tiles_json)
-
-    _results: dict
+            save_json(self.tiles_seen, self.user_seen_tiles_json)
 
 
 if __name__ == '__main__':
